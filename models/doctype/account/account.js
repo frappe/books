1c9b43225bdac0ec2e78d4d1fd40a929c518ebe9
Rushabh Mehta <rmehta@gmail.com>
2018-01-23 18:17:56 +0530

    overrride link filters

--- models/doctype/account/account.js
+++ models/doctype/account/account.js
@@ -4,12 +4,21 @@ class account_meta extends frappe.meta.Meta {
     setup_meta() {
         Object.assign(this, require('./account.json'));
         this.list_options.fields = ['name', 'account_type'];
+        this.setup_links();
+    }
+
+    setup_links() {
+        this.get_field('parent_account').get_filters = function(query) {
+            return {
+                keywords: ["like", query],
+                name: ["!=", this.form.doc.name]
+            }
+        }
     }
 
     get_row_html(data) {
         return `<a href="#edit/account/${data.name}">${data.name} (${data.account_type})</a>`;
     }
-
 }
 
 class account extends frappe.document.Document {
