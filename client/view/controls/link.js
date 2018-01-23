c77f319d817f6ba14128615dc4d4c8befd5903cd
Rushabh Mehta <rmehta@gmail.com>
2018-01-23 18:01:09 +0530

    tabs to spaces

--- client/view/controls/link.js
+++ client/view/controls/link.js
@@ -5,21 +5,21 @@ const Awesomplete = require('awesomplete');
 class LinkControl extends BaseControl {
     make() {
         super.make();
-		this.input.setAttribute('type', 'text');
-		this.awesomplete = new Awesomplete(this.input, {
-			autoFirst: true,
-			minChars: 0,
-			maxItems: 99
-		});
+        this.input.setAttribute('type', 'text');
+        this.awesomplete = new Awesomplete(this.input, {
+            autoFirst: true,
+            minChars: 0,
+            maxItems: 99
+        });
 
-		// rebuild the list on input
-		this.input.addEventListener('input', async (event) => {
-			this.awesomplete.list = (await frappe.db.get_all({
-				doctype: this.options,
-				filters: {keywords: ["like", this.input.value]},
-				limit: 50
-			})).map(d => d.name);
-		});
+        // rebuild the list on input
+        this.input.addEventListener('input', async (event) => {
+            this.awesomplete.list = (await frappe.db.get_all({
+                doctype: this.options,
+                filters: { keywords: ["like", this.input.value] },
+                limit: 50
+            })).map(d => d.name);
+        });
     }
 };
 
