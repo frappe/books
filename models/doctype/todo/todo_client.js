a53cf5a26459bed57544b0fe7993a509a4666752
Rushabh Mehta <rmehta@gmail.com>
2018-01-24 17:25:22 +0530

    [refactor] extending forms and lists

--- models/doctype/todo/todo_client.js
+++ models/doctype/todo/todo_client.js
@@ -2,15 +2,15 @@ const BaseList = require('frappejs/client/view/list');
 const BaseForm = require('frappejs/client/view/form');
 
 class ToDoList extends BaseList {
-	get_fields()  {
-		return ['name', 'subject', 'status'];
-	}
-	get_row_html(data) {
+    get_fields()  {
+        return ['name', 'subject', 'status'];
+    }
+    get_row_html(data) {
         return `<a href="#edit/todo/${data.name}">${data.subject}</a>`;
-	}
+    }
 }
 
 module.exports = {
-	Form: BaseForm,
-	List: ToDoList
+    Form: BaseForm,
+    List: ToDoList
 }
