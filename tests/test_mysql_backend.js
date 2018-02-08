ab219281427ff75d124a526fbf23d43e37cf353c
Shridhar <shridhar.p@zerodha.com>
2018-02-08 16:25:57 +0530

    updated travis yml for mysql backend
    travis: Create mysql database
    runs mysql backend tests

---
+++ tests/test_mysql_backend.js
@@ -0,0 +1,11 @@
+const server = require('frappejs/server');
+
+server.start({
+    backend: 'mysql',
+    connection_params: {
+	host     : "test_frappejs",
+        username : "test_frappejs",
+        password : "test_frappejs",
+        db_name  : "test_frappejs"
+    }
+});
