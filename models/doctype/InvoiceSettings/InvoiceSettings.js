972fb9517e3e1a306f14484a23ccc0cfebc1de8f
Rushabh Mehta <rmehta@gmail.com>
2018-02-19 22:12:07 +0530

    socketio

--- models/doctype/InvoiceSettings/InvoiceSettings.js
+++ models/doctype/InvoiceSettings/InvoiceSettings.js
@@ -1,17 +1,17 @@
 module.exports = {
-	"name": "InvoiceSettings",
-	"label": "Invoice Settings",
-	"doctype": "DocType",
-	"isSingle": 1,
-	"isChild": 0,
-	"keywordFields": [],
-	"fields": [
-		{
-			"fieldname": "numberSeries",
-			"label": "Number Series",
-			"fieldtype": "Link",
-			"target": "NumberSeries",
-			"required": 1
-		}
-	]
+    "name": "InvoiceSettings",
+    "label": "Invoice Settings",
+    "doctype": "DocType",
+    "isSingle": 1,
+    "isChild": 0,
+    "keywordFields": [],
+    "fields": [
+        {
+            "fieldname": "numberSeries",
+            "label": "Number Series",
+            "fieldtype": "Link",
+            "target": "NumberSeries",
+            "required": 1
+        }
+    ]
 }
