34131c86d76f08b775df1fd4de788d71a739d5dd
Rushabh Mehta <rmehta@gmail.com>
2018-02-21 15:13:34 +0530

    whitespace

--- models/doctype/Tax/Tax.js
+++ models/doctype/Tax/Tax.js
@@ -1,22 +1,22 @@
 module.exports = {
-	"name": "Tax",
-	"doctype": "DocType",
-	"isSingle": 0,
-	"isChild": 0,
-	"keywordFields": ["name"],
-	"fields": [
-		{
-			"fieldname": "name",
-			"label": "Name",
-			"fieldtype": "Data",
-			"required": 1
-		},
-		{
-			"fieldname": "details",
-			"label": "Details",
-			"fieldtype": "Table",
-			"childtype": "TaxDetail",
-			"required": 1
-		}
-	]
+    "name": "Tax",
+    "doctype": "DocType",
+    "isSingle": 0,
+    "isChild": 0,
+    "keywordFields": ["name"],
+    "fields": [
+        {
+            "fieldname": "name",
+            "label": "Name",
+            "fieldtype": "Data",
+            "required": 1
+        },
+        {
+            "fieldname": "details",
+            "label": "Details",
+            "fieldtype": "Table",
+            "childtype": "TaxDetail",
+            "required": 1
+        }
+    ]
 }
