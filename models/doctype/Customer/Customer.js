fb97ad5720560b770e5cafcaee2a0416808640f2
Rushabh Mehta <rmehta@gmail.com>
2018-02-21 15:13:34 +0530

    whitespace

--- models/doctype/Customer/Customer.js
+++ models/doctype/Customer/Customer.js
@@ -1,17 +1,17 @@
 module.exports = {
-	"name": "Customer",
-	"doctype": "DocType",
-	"isSingle": 0,
-	"istable": 0,
-	"keywordFields": [
-		"name"
-	],
-	"fields": [
-		{
-			"fieldname": "name",
-			"label": "Name",
-			"fieldtype": "Data",
-			"required": 1
-		}
-	]
+    "name": "Customer",
+    "doctype": "DocType",
+    "isSingle": 0,
+    "istable": 0,
+    "keywordFields": [
+        "name"
+    ],
+    "fields": [
+        {
+            "fieldname": "name",
+            "label": "Name",
+            "fieldtype": "Data",
+            "required": 1
+        }
+    ]
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
 module.exports = [
-	require('frappejs/config/rollup.config.style.js'),
-	require('frappejs/config/rollup.config.app.js')
+    require('frappejs/config/rollup.config.style.js'),
+    require('frappejs/config/rollup.config.app.js')
 ]
