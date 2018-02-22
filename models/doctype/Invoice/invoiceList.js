db83ed8a7a8f5a05805342d84146b09471e22377
Rushabh Mehta <rmehta@gmail.com>
2018-02-22 16:52:51 +0530

    list view updates

--- models/doctype/Invoice/invoiceList.js
+++ models/doctype/Invoice/invoiceList.js
@@ -6,8 +6,8 @@ module.exports = class InvoiceList extends BaseList {
         return ['name', 'customer', 'grandTotal'];
     }
     getRowHTML(data) {
-        return `<div class="col-2">${data.name}</div>
-                <div class="col-5 text-muted">${data.customer}</div>
+        return `<div class="col-3">${this.getNameHTML(data)}</div>
+                <div class="col-4 text-muted">${data.customer}</div>
                 <div class="col-4 text-muted text-right">${frappe.format(data.grandTotal, "Currency")}</div>`;
     }
 }
