1eb7a7b59be5543a555f7948f4dfaae8752bf920
Prateeksha Singh <pratu16x7@gmail.com>
2018-03-29 20:15:02 +0530

    comment setup

--- www/index.js
+++ www/index.js
@@ -7,15 +7,16 @@ client.start({
     server: 'localhost:8000',
     makeDesk: 0
 }).then(() => {
-    new SetupWizard({
-        postSetup: async (data) => {
-            console.log(data);
-            client.makeDesk(3);
-            appClient.start();
+    // new SetupWizard({
+    //     postSetup: async (data) => {
+    //         client.makeDesk(3);
+    //         appClient.start();
 
-            await frappe.router.setRoute('list', 'ToDo');
-        }
-    });
+    //         await frappe.router.setRoute('list', 'ToDo');
+    //     }
+    // });
+    client.makeDesk(3);
+    appClient.start();
 });
 
 module.exports = false;
