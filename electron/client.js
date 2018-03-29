7be87aa806c7f2dc50109bcd57d57ef993f87265
Faris Ansari <netchamp.faris@gmail.com>
2018-03-30 00:22:46 +0530

    Add electron commands and files

--- electron/client.js
+++ electron/client.js
@@ -1,33 +1,12 @@
-const client = require('frappejs/client/electron');
+const electron = require('frappejs/client/electron');
+const appClient = require('../client');
 
-// start server
-client.start({
+electron.start({
     dbPath: 'test.db',
-    columns: 3
-}).then(() => {
-
-    // require modules
-    frappe.registerModels(require('../models'), 'client');
-
-    frappe.registerView('List', 'ToDo', require('frappejs/models/doctype/ToDo/ToDoList.js'));
-    frappe.registerView('Form', 'FilterSelector', require('frappejs/models/doctype/FilterSelector/FilterSelectorForm.js'));
-
-    frappe.registerView('List', 'Account', require('../models/doctype/Account/AccountList.js'));
-    frappe.registerView('Form', 'Account', require('../models/doctype/Account/AccountForm.js'));
-
-    frappe.registerView('List', 'Invoice', require('../models/doctype/Invoice/InvoiceList.js'));
-    frappe.registerView('List', 'Customer', require('../models/doctype/Party/CustomerList.js'));
-
-    frappe.desk.menu.addItem('ToDo', '#list/ToDo');
-    frappe.desk.menu.addItem('Accounts', '#list/Account');
-    frappe.desk.menu.addItem('Items', '#list/Item');
-    frappe.desk.menu.addItem('Customers', '#list/Customer');
-    frappe.desk.menu.addItem('Invoice', '#list/Invoice');
-    frappe.desk.menu.addItem('Settings', () => frappe.desk.showFormModal('SystemSettings'));
-
-    frappe.router.default = '#list/ToDo';
-
-    frappe.router.show(window.location.hash);
-});
+    columns: 3,
+    models: require('../models')
+}).then((frappe) => {
 
 module.exports = false;
+    appClient.start(frappe);
+});
