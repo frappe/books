890acbe26f500b19524895ce1c385f0ef58d4f05
Faris Ansari <netchamp.faris@gmail.com>
2018-03-30 22:32:28 +0530

    refactor setupwizard

--- setup/config.js
+++ setup/config.js
@@ -1,55 +1,86 @@
-module.exports = [
-	{
-		fields: [
-			{
-				"fieldname": "country",
-				"label": "Country",
-				"fieldtype": "Data",
-				"required": 1
-			}
-		],
-		title: 'Select Country'
-	},
+const countryList = Object.keys(require('../fixtures/countryInfo.json')).sort();
 
-	{
-		fields: [
-			{
-				"fieldname": "name",
-				"label": "Name",
-				"fieldtype": "Data",
-				"required": 1
-			},
-			{
-				"fieldname": "email",
-				"label": "Email",
-				"fieldtype": "Data",
-				"required": 1
-			}
-		],
-		title: 'Add a Profile'
-	},
+module.exports = {
+    fields: [
 
-	{
-		fields: [
-			{
-				"fieldname": "companyName",
-				"label": "Company Name",
-				"fieldtype": "Data",
-				"required": 1
-			},
-			{
-				"fieldname": "abbreviation",
-				"label": "Abbreviation",
-				"fieldtype": "Data",
-				"required": 1
-			},
-			{
-				"fieldname": "bankName",
-				"label": "Bank Name",
-				"fieldtype": "Data",
-				"required": 1
-			}
-		],
-		title: 'Add your Company'
-	}
-]
+        {
+            "fieldname": "file",
+            "label": "File",
+            "fieldtype": "File",
+            "required": 1,
+            "directory": 1
+        },
+
+        {
+            "fieldname": "country",
+            "label": "Country",
+            "fieldtype": "Autocomplete",
+            "required": 1,
+            getList: () => countryList
+        },
+
+        {
+            "fieldname": "name",
+            "label": "Name",
+            "fieldtype": "Data",
+            "required": 1
+        },
+
+        {
+            "fieldname": "email",
+            "label": "Email",
+            "fieldtype": "Data",
+            "required": 1
+        },
+
+        {
+            "fieldname": "companyName",
+            "label": "Company Name",
+            "fieldtype": "Data",
+            "required": 1
+        },
+
+        {
+            "fieldname": "abbreviation",
+            "label": "Abbreviation",
+            "fieldtype": "Data",
+            "required": 1
+        },
+
+        {
+            "fieldname": "bankName",
+            "label": "Bank Name",
+            "fieldtype": "Data",
+            "required": 1
+        }
+    ],
+
+    layout: [
+        {
+            title: 'Select File location',
+            fields: ['file']
+        },
+
+        {
+            title: 'Select Country',
+            fields: ['country']
+        },
+
+        {
+            title: 'Add a Profile',
+            fields: ['name', 'email']
+        },
+
+        {
+            title: 'Add your Company',
+            columns: [
+                {
+                    fields: ['companyName', 'bankName']
+                },
+                {
+                    fields: ['abbreviation']
+                },
+            ]
+        }
+    ]
+}
