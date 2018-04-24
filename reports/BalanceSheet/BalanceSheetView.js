180825243f794d2dca84e75c66b3532a73488a80
Faris Ansari <netchamp.faris@gmail.com>
2018-04-24 13:28:57 +0530

    Reports
    - Common FinancialStatementsView
    - Add BalanceSheet
    - Add Fiscal Year in Accounting Settings

--- reports/BalanceSheet/BalanceSheetView.js
+++ reports/BalanceSheet/BalanceSheetView.js
@@ -1,13 +1,12 @@
 const frappe = require('frappejs');
 const FinancialStatementsView = require('../FinancialStatements/FinancialStatementsView');
 
-module.exports = class ProfitAndLossView extends FinancialStatementsView {
+module.exports = class BalanceSheetView extends FinancialStatementsView {
     constructor() {
         super({
-            title: frappe._('Profit and Loss'),
-            method: 'profit-and-loss',
+            title: frappe._('Balance Sheet'),
+            method: 'balance-sheet',
             filterFields: [
-                {fieldtype: 'Date', label: 'From Date', required: 1},
                 {fieldtype: 'Date', label: 'To Date', required: 1},
                 {fieldtype: 'Select', options: ['Monthly', 'Quarterly', 'Half Yearly', 'Yearly'],
                     label: 'Periodicity', fieldname: 'periodicity', default: 'Monthly'}
