90f8c754a9f26aa20a195dbc8bca69f4b63e85a2
Faris Ansari <netchamp.faris@gmail.com>
2018-06-04 14:58:45 +0530

    NotFound and FeatherIcon component

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import NotFound from './components/NotFound';
+import FeatherIcon from './components/FeatherIcon';
 
 Vue.config.productionTip = false
 
 Vue.component('not-found', NotFound);
+Vue.component('feather-icon', FeatherIcon);
 
 /* eslint-disable no-new */
 new Vue({
