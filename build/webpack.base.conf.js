405e9241df54de217a23529b5b68a67c1e358d90
Faris Ansari <netchamp.faris@gmail.com>
2018-06-04 15:34:01 +0530

    Handle deepmerge default export

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,7 +35,8 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'deepmerge$': 'deepmerge/dist/umd.js'
     }
   },
   module: {
