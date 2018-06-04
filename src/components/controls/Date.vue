d6989ff2dd81453945ac40645d8a3eac60969cc2
Faris Ansari <netchamp.faris@gmail.com>
2018-06-04 16:30:33 +0530

    Add Date control

---
+++ src/components/controls/Date.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="form-group">
+    <label>{{ docfield.label }}</label>
+    <flat-pickr
+      :value="value"
+      class="form-control"
+      @on-change="emitChange"
+    >
+    </flat-pickr>
+  </div>
+</template>
+<script>
+import flatPickr from 'vue-flatpickr-component';
+import Data from './Data';
+
+export default {
+  extends: Data,
+  components: {
+    flatPickr
+  },
+  methods: {
+    emitChange(dates, dateString) {
+      this.$emit('change', dateString);
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "flatpickr/dist/flatpickr.css";
+</style>
