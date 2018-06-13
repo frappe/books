fdfd227443a702de4314936138284af5edb923d2
karan28598 <mr.karanagrawal@gmail.com>
2018-06-13 18:48:42 +0530

    integrate create-invoice vue component

---
+++ src/components/CreateInvoice.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="list-group">
+        <button class="list-group-item item" @click="createInvoice()">
+            <strong>Create Invoice</strong>
+        </button>
+    </div>
+</template>
+<script>
+export default {
+  /*methods: {
+      createInvoice(//todo) {
+          //todo
+      }
+  }*/
+};
+</script>
