0eb1be2b364a90d1298b565103a37299ccaadb8e
Chirag Shetty <chirag.shetty@somaiya.edu>
2018-06-18 13:24:16 +0530

    Vue dev (#12)
    * Add Numpad component
    
    * add customer selection
    
    * Style - Customer Select Field
    
    * Add Item Search and Item Group
    
    * stashing
    
    * Resolve customer and item search conflict
    
    * Remove item group
    
    * Handle no items selected
    
    * create temp invoice object for calc
    
    * Change Double click item edit to single click
    
    * prevent invoice without customer

---
+++ src/components/Numpad.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="numpad">
+    <input type="text" v-model="amount"><br/>
+    <div class="btn-group-vertical ml-4 mt-4" role="group" aria-label="Basic example">
+      <div class="btn-group">
+        <button @click="append(1)" class="btn btn-outline-secondary border-bottom-0 rounded-0">1</button>
+        <button @click="append(2)" class="btn btn-outline-secondary border-bottom-0">2</button>
+        <button @click="append(3)" class="btn btn-outline-secondary border-bottom-0 rounded-0">3</button>
+      </div>
+      <div class="btn-group">
+        <button @click="append(4)" class="btn btn-outline-secondary border-bottom-0 rounded-0">4</button>
+        <button @click="append(5)" class="btn btn-outline-secondary border-bottom-0">5</button>
+        <button @click="append(6)" class="btn btn-outline-secondary border-bottom-0 rounded-0">6</button>
+      </div>
+      <div class="btn-group">
+        <button @click="append(7)" class="btn btn-outline-secondary border-bottom-0 rounded-0">7</button>
+        <button @click="append(8)" class="btn btn-outline-secondary border-bottom-0">8</button>
+        <button @click="append(9)" class="btn btn-outline-secondary border-bottom-0 rounded-0">9</button>
+      </div>
+      <div class="btn-group">
+        <button @click="del()" class="btn btn-outline-secondary rounded-0">Del</button>
+        <button @click="append(0)" class="btn btn-outline-secondary">0</button>
+        <button @click="addDecimalPoint()" class="btn btn-outline-secondary rounded-0">.</button>
+	    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "numpad",
+  data() {
+    return {
+      amount: ""
+    };
+  },
+  methods: {
+    append: function(value) {
+      this.amount += value;
+    },
+    del: function() {
+      this.amount = this.amount.slice(0, -1);
+    },
+    addDecimalPoint: function(value) {
+      if (!this.amount.includes(".")) this.amount += ".";
+    }
+  }
+};
+</script>
+<style scoped>
+.btn {
+    font-size: 20px;
+    width:64px;
+    height:64px;
+}
+</style>
