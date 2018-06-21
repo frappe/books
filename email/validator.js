497cb60d29d63757c177126be8c2b1b906471ecf
Nishchith K <inishchith@gmail.com>
2018-06-21 10:57:08 +0530

    Error Handling : invalid email id  (#17)
    * Update (#1)
    
    *  fix sender name bug
    
    *  add emailSyncOption remove email model title
    
    *  handle error : invalid email + quick fix

---
+++ email/validator.js
@@ -0,0 +1,9 @@
+module.exports = {
+    validate: async function (mailDetails) {
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if (reg.test(mailDetails.email) == false) {
+            return false;
+        }
+        return true;
+    }
+}
