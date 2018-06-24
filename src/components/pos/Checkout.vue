2429aca4fc70ed5ea724f7275ec8995bf6450664
Jainam C Shah <jainamcshah98@gmail.com>
2018-06-24 23:10:52 +0530

    Integrate Numpad into Checkout component

--- src/components/pos/Checkout.vue
+++ src/components/pos/Checkout.vue
@@ -1,15 +1,15 @@
 <template>
 <div class="row">
   <div class="col-md-6">
+    <h6>Cash</h6>
+    <input v-model="cash" @click="() => changeFocus(this.inputFields.cash)">
     <h6>Cheque</h6>
-    <input v-model="cheque">
+    <input v-model="cheque" @click="() => changeFocus(this.inputFields.cheque)">
     <h6>Outstanding</h6>
     <input v-model="outstanding">
-    <h6>Cash</h6>
-    <input v-model="cash">
   </div>
   <div class="col-md-6">
-    <numpad></numpad>
+    <numpad :appendNum="append" :addDecimalPoint="addDP" :delNum="del"></numpad>
   </div>
 </div>
 </template>
@@ -20,14 +20,45 @@ export default {
     components: {
       Numpad
     },
-    data: {
-        cash: 0,
-        cheque: 0,
-        outstanding: 0
+    data() {
+      return {
+
+        cash: "",
+				cheque: "",
+        outstanding: 0,
+				focused: "",
+				inputFields: {
+          cash: "CASH",
+					cheque: "CHEQUE"
+				}
+      }
+    },
+    methods: {
+      append: function(value){
+				switch(this.focused){
+					case "CASH": this.cash += value; break;
+					case "CHEQUE": this.cheque += value; break;
+				}	
+			},
+			del: function() {
+				switch(this.focused){
+					case "CASH": this.cash = this.cash.slice(0, -1); break;
+					case "CHEQUE": this.cheque = this.cheque.slice(0, -1);; break;
+				}
+    	},
+    	addDP: function(value) {
+				switch(this.focused){
+					case "CASH": if (!this.cash.includes(".")) this.cash += "."; break;
+					case "CHEQUE": if (!this.cheque.includes(".")) this.cheque += "."; break;
+				}
+   	 	},
+			changeFocus: function(target){
+				this.focused = target;
+			}
     },
     computed: {
         outstandingAmount(){
-            outstanding =  grandTotal - cash - cheque
+            this.outstanding =  grandTotal - parseFloat(this.cash) - parseFloat(this.cheque)
         }
     }
 }
