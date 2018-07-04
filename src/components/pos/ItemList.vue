8893e404726a310044aadbc6ddb4ebb546df149a
karan28598 <mr.karanagrawal@gmail.com>
2018-07-04 19:38:11 +0530

    change item list-view to grid-view

--- src/components/pos/ItemList.vue
+++ src/components/pos/ItemList.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="list-group">
-        <button class="list-group-item item" v-for="item in items" :key="item.name" @click="itemClicked(item)">
-            <strong>{{ item.name }}</strong> - {{ item.rate }}
+    <div class="row">
+        <button class="list-group-item col-sm-4" v-for="item in items" :key="item.name" @click="itemClicked(item)"> 
+            <strong>{{ item.name }}</strong><br>
+            <img src="../images/mobile.jpeg"><br>
+            <strong>INR</strong> - {{ item.rate }}
         </button>
     </div>
 </template>
+
+<style lang="scss">
+img {
+    height: 150px;
+    width: 100px;
+}
+</style>
+
 <script>
 export default {
   props: ["items", "add"],
