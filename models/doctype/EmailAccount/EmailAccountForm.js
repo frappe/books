d34f331aa41f15c0be441a5359c2616e1ad84428
Aakanksha Jain <aakanksha.jain8@gmail.com>
2018-07-08 20:41:54 +0530

    Confirm password reverted
    To be done on `move-to-vue`

--- models/doctype/EmailAccount/EmailAccountForm.js
+++ models/doctype/EmailAccount/EmailAccountForm.js
@@ -1,7 +1,5 @@
 const BaseForm = require('frappejs/client/view/form');
 const frappe = require('frappejs');
-const getConfig = require("../../../email/getConfig");
-
 
 module.exports = class EmailAccountForm extends BaseForm {
     constructor({ doctype, parent, submit_label='', container, meta, inline=false }) {
@@ -10,17 +8,14 @@ module.exports = class EmailAccountForm extends BaseForm {
     }
 
     makeSaveButton() {
-       
         this.saveButton = this.container.addButton(frappe._("Save"), 'primary', async (event) => {
-            let account = await getConfig();
-            
             var response = await frappe.call({
                 method: 'validate-mail',
                 args: this.doc.getValidDict()
             });
             if(response == true){
                 await this.save();
-                /*
+                 /*
                 let x = await frappe.call({
                     method: 'validate-auth',
                     args: userInput 
@@ -37,15 +32,10 @@ module.exports = class EmailAccountForm extends BaseForm {
             else{
                 frappe.ui.showAlert({message: frappe._('Failed : Invalid Email'), color: 'red'});
             }
-            if(account[0].password === account[0].confirmPassword) {
-                       console.log('haha');
-            }
-            else
-            { frappe.ui.showAlert({message: frappe._('Error : Passwords do not match'), color: 'red'}); } 
-    });
+        });
         this.on('change', () => {
             const show = this.doc._dirty && !this.doc.submitted;
             this.saveButton.classList.toggle('hide', !show);
         });
-     }
+    }
 };
