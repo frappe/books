72de3b716f4d3349653e5c4d77c69b1bbcd0617a
Aakanksha Jain <aakanksha.jain8@gmail.com>
2018-07-08 11:54:19 +0530

    Passwords do not match alert added

--- models/doctype/EmailAccount/EmailAccountForm.js
+++ models/doctype/EmailAccount/EmailAccountForm.js
@@ -1,5 +1,7 @@
 const BaseForm = require('frappejs/client/view/form');
 const frappe = require('frappejs');
+const getConfig = require("../../../email/getConfig");
+
 
 module.exports = class EmailAccountForm extends BaseForm {
     constructor({ doctype, parent, submit_label='', container, meta, inline=false }) {
@@ -8,14 +10,16 @@ module.exports = class EmailAccountForm extends BaseForm {
     }
 
     makeSaveButton() {
+       
         this.saveButton = this.container.addButton(frappe._("Save"), 'primary', async (event) => {
-            let userInput = this.doc.getValidDict();
+            let account = await getConfig();
+            if(account[0].password == account[0].confirmPassword) {
             var response = await frappe.call({
                 method: 'validate-mail',
-                args: userInput
+                args: this.doc.getValidDict()
             });
             if(response == true){
-                this.save();
+                await this.save();
                 /*
                 let x = await frappe.call({
                     method: 'validate-auth',
@@ -33,10 +37,13 @@ module.exports = class EmailAccountForm extends BaseForm {
             else{
                 frappe.ui.showAlert({message: frappe._('Failed : Invalid Email'), color: 'red'});
             }
-        });
+        }
+            else
+            { frappe.ui.showAlert({message: frappe._('Error : Passwords do not match'), color: 'red'}); } 
+    });
         this.on('change', () => {
             const show = this.doc._dirty && !this.doc.submitted;
             this.saveButton.classList.toggle('hide', !show);
         });
-    }
+     }
 };
