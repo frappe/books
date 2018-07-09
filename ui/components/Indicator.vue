5375f21d302509fd8a47169983e37bcbe32a6cf8
Faris Ansari <netchamp.faris@gmail.com>
2018-07-09 20:25:24 +0530

    Indicators
    - Replace hardcoded color values with constants
    - Add remaining color styles in Indicator component
    - Show Indicator in List View

--- ui/components/Indicator.vue
+++ ui/components/Indicator.vue
@@ -2,8 +2,24 @@
     <span :class="['indicator', 'indicator-' + color]"></span>
 </template>
 <script>
+import indicatorColor from 'frappejs/ui/constants/indicators';
+
 export default {
-    props: ['color']
+    props: {
+        color: {
+            type: String,
+            required: true,
+            default: indicatorColor.GREY,
+            validator(value) {
+                const validColors = Object.values(indicatorColor);
+                const valid = validColors.includes(value);
+                if (!valid) {
+                    console.warn(`color must be one of `, validColors);
+                }
+                return valid;
+            }
+        }
+    }
 }
 </script>
 
@@ -14,11 +30,31 @@ export default {
     display: inline-block;
     width: 0.5rem;
     height: 0.5rem;
-    background-color: $gray-400;
     border-radius: 50%;
 }
 
+.indicator-grey {
+    background-color: $gray-300;
+}
 .indicator-blue {
-    background-color: $primary;
+    background-color: $blue;
+}
+.indicator-red {
+    background-color: $red;
+}
+.indicator-green {
+    background-color: $green;
+}
+.indicator-orange {
+    background-color: $orange;
+}
+.indicator-purple {
+    background-color: $purple;
+}
+.indicator-yellow {
+    background-color: $yellow;
+}
+.indicator-black {
+    background-color: $gray-800;
 }
 </style>
---
+++ ui/constants/indicators.js
@@ -0,0 +1,11 @@
+module.exports = {
+    GRAY: 'grey',
+    GREY: 'grey',
+    BLUE: 'blue',
+    RED: 'red',
+    GREEN: 'green',
+    ORANGE: 'orange',
+    PURPLE: 'purple',
+    YELLOW: 'yellow',
+    BLACK: 'black',
+}
