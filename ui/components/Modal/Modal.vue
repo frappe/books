3263fbebf4fa0af2f7b15a9d2dfcd2bfee840b93
Faris Ansari <netchamp.faris@gmail.com>
2018-07-10 19:05:43 +0530

    Modal
    - Simplify modal plugin
    - Support multiple stacked modals in a modal container
    - Add formModal plugin

---
+++ ui/components/Modal/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="['modal fade show d-block']"
+    tabindex="-1" role="dialog" aria-labelledby="frappe-modal-label" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content shadow">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="closeModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body modal-height">
+          <component :is="component" v-bind="props" v-on="events"/>
+        </div>
+        <div class="modal-footer">
+          <f-button secondary @click="closeModal">{{ _('Close') }}</f-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Modal Title"
+    },
+    component: {
+      type: Object
+    },
+    props: {
+      type: Object
+    },
+    events: {
+      type: Object
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close-modal');
+    }
+  }
+};
+</script>
+<style scoped>
+.modal-height {
+  max-height: 80vh;
+  overflow: auto;
+}
+</style>
---
+++ ui/components/Modal/plugin.js
@@ -0,0 +1,25 @@
+import ModalContainer from './ModalContainer';
+
+const Plugin = {
+  install (Vue) {
+
+    this.event = new Vue();
+
+    Vue.prototype.$modal = {
+      show(...args) {
+        Plugin.modalContainer.add(...args);
+      },
+
+      hide(id) {
+        Plugin.event.$emit('hide', id);
+      }
+    }
+
+    // create modal container
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    new Vue({ render: h => h(ModalContainer) }).$mount(div);
+  }
+}
+
+export default Plugin;
