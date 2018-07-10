d654f89595d7cedd47819f8450a9486a6aa4c0a2
Faris Ansari <netchamp.faris@gmail.com>
2018-07-10 19:10:23 +0530

    Update modalPlugin

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <router-view />
     </frappe-desk>
     <router-view v-else name="setup" />
-    <frappe-modal ref="modal" :show="modalVisible" v-bind="modalOptions" @close-modal="modalVisible = false"/>
   </div>
 </template>
 
@@ -12,7 +11,6 @@
 import Vue from 'vue';
 import Observable from 'frappejs/utils/observable';
 import Desk from 'frappejs/ui/components/Desk';
-import Modal from 'frappejs/ui/components/Modal';
 import sidebarConfig from './sidebarConfig';
 
 export default {
@@ -25,7 +23,6 @@ export default {
   },
   components: {
     FrappeDesk: Desk,
-    FrappeModal: Modal
   },
   async beforeRouteUpdate(to, from, next) {
     const accountingSettings = await frappe.getSingle('AccountingSettings');
