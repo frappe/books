8bff2e42fbaab2dcd1d56bd26644be69d6812a18
Suraj Shetty <surajshetty3416@gmail.com>
2018-07-12 20:47:46 +0530

    Filter list based on search value

--- ui/components/Navbar.vue
+++ ui/components/Navbar.vue
@@ -1,7 +1,32 @@
 <template>
-    <nav class="frappe-navbar navbar navbar-light bg-light row no-gutters border-bottom border-top">
-        <form class="form-inline col-4 pr-3">
-            <input type="search" name="search" class="form-control shadow-none w-100" placeholder="Search...">
-        </form>
-    </nav>
+	<nav class="frappe-navbar navbar navbar-light bg-light row no-gutters border-bottom border-top">
+		<form v-if="showSearch" class="form-inline col-4 pr-3">
+			<input type="text"
+				v-model="searchValue"
+				@input="updateSearch"
+				name="search"
+				class="form-control shadow-none w-100"
+				placeholder="Search...">
+		</form>
+		<div class="navbar-text">HI</div>
+	</nav>
 </template>
+<script>
+import { debounce } from 'lodash';
+export default {
+    props: ["showSearch"],
+    data() {
+        return {
+            searchValue: ''
+        };
+    },
+    mounted() {
+        this.$root.$on('newList', () => this.searchValue = '')
+    },
+    methods: {
+        updateSearch: debounce(function() {
+            this.$root.$emit("search", this.searchValue);
+        }, 500)
+    }
+};
+</script>
