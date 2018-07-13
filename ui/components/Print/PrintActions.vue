70e985485ce6ae8d0d1fa5a7c035e5065a113515
Prateeksha Singh <pratu16x7@gmail.com>
2018-07-13 13:15:30 +0530

    [print-view] fix button layout

--- ui/components/Print/PrintActions.vue
+++ ui/components/Print/PrintActions.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="frappe-print-actions d-flex justify-content-between align-items-center p-3 border-bottom">
-    <h5 class="m-0">{{ title }}</h5>
-    <f-button primary @click="$emit('print')">{{ _('Print') }}</f-button>
-    <f-button secondary @click="$emit('view-form')">{{ _('View Form') }}</f-button>
+    <h5 class="m-0">Print {{ title }}</h5>
+
+    <div class="button-group">
+      <f-button secondary @click="$emit('view-form')">{{ _('View Form') }}</f-button>
+      <f-button primary @click="$emit('print')">{{ _('Print') }}</f-button>
+    </div>
   </div>
 </template>
 
