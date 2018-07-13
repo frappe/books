9ce36bb40c89df259ff25cc4488d8ea113fd8446
Prateeksha Singh <pratu16x7@gmail.com>
2018-07-13 12:59:57 +0530

    [print] print actions and layout

--- ui/pages/ListAndPrintView.vue
+++ ui/pages/ListAndPrintView.vue
@@ -3,14 +3,14 @@
         <div class="col-4 border-right">
             <frappe-list :doctype="doctype" :filters="filters" :key="doctype" />
         </div>
-        <div class="col-8">
-            <print-view v-if="name" :key="doctype + name" :doctype="doctype" :name="name" @save="onSave" />
+        <div class="col-8 background-gray">
+            <print-view v-if="name" :key="doctype + name" :doctype="doctype" :name="name" />
         </div>
     </div>
 </template>
 <script>
 import List from '../components/List/List';
-import PrintView from '../components/PrintView';
+import PrintView from '../components/Print/PrintView';
 
 export default {
     props: ['doctype', 'name', 'filters'],
@@ -20,8 +20,12 @@ export default {
     },
 }
 </script>
-<style>
+<style lang="scss">
 .frappe-list-form {
     min-height: calc(100vh - 4rem);
+}
+@import "../styles/variables";
+.background-gray {
+    background-color: $gray-100
 }
 </style>
