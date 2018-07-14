a9e6695e824d90721ec50ec04857b088c3ad6b64
Suraj Shetty <surajshetty3416@gmail.com>
2018-07-14 20:28:18 +0530

    Add next action links for doctype meta

--- ui/components/Form/FormActions.vue
+++ ui/components/Form/FormActions.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="frappe-form-actions d-flex justify-content-between align-items-center p-3 border-bottom">
     <h5 class="m-0">{{ title }}</h5>
-    <f-button primary v-if="isDirty" @click="$emit('save')">{{ _('Save') }}</f-button>
-    <f-button primary v-if="showSubmit" @click="$emit('submit')">{{ _('Submit') }}</f-button>
-    <f-button secondary v-if="showRevert" @click="$emit('revert')">{{ _('Revert') }}</f-button>
+    <div class="d-flex">
+      <f-button primary v-if="isDirty" @click="$emit('save')">{{ _('Save') }}</f-button>
+      <f-button primary v-if="showSubmit" @click="$emit('submit')">{{ _('Submit') }}</f-button>
+      <f-button secondary v-if="showRevert" @click="$emit('revert')">{{ _('Revert') }}</f-button>
+      <dropdown v-if="links.length" :label="'Next Action'" :options="links"></dropdown>
+    </div>
   </div>
 </template>
 <script>
 import frappe from 'frappejs';
+import Dropdown from '../Dropdown';
 
 export default {
-  props: ['doc'],
+  props: ['doc', 'links'],
+  components: {
+    Dropdown
+  },
   data() {
     return {
       isDirty: false,
