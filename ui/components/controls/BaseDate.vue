dc602d3424e42ffad5567a882189d55d6b0b3f04
Faris Ansari <netchamp.faris@gmail.com>
2018-07-14 12:46:40 +0530

    Commonify Date and Time component into BaseDate component

---
+++ ui/components/controls/BaseDate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="{'form-group': !onlyInput}">
+    <label v-if="!onlyInput">{{ docfield.label }}</label>
+    <flat-pickr
+      class="form-control"
+      :value="value"
+      :config="config"
+      @on-change="emitChange">
+    </flat-pickr>
+  </div>
+</template>
+<script>
+import flatPickr from 'vue-flatpickr-component';
+import Data from './Data';
+
+export default {
+  extends: Data,
+  props: ['config'],
+  components: {
+    flatPickr
+  },
+  methods: {
+    emitChange(dates, dateString) {
+      this.$emit('change', dateString);
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "~flatpickr/dist/flatpickr.css";
+</style>
--- ui/components/controls/Date.vue
+++ ui/components/controls/Date.vue
@@ -1,29 +1,13 @@
-<template>
-  <div :class="{'form-group': !onlyInput}">
-    <label v-if="!onlyInput">{{ docfield.label }}</label>
-    <flat-pickr
-      :value="value"
-      class="form-control"
-      @on-change="emitChange">
-    </flat-pickr>
-  </div>
-</template>
 <script>
-import flatPickr from 'vue-flatpickr-component';
-import Data from './Data';
+import BaseDate from './BaseDate';
 
 export default {
-  extends: Data,
-  components: {
-    flatPickr
-  },
-  methods: {
-    emitChange(dates, dateString) {
-      this.$emit('change', dateString);
+  extends: BaseDate,
+  props: {
+    config: {
+      type: Object,
+      default: () => ({})
     }
   }
 };
 </script>
-<style lang="scss">
-@import "flatpickr/dist/flatpickr.css";
-</style>
