9d040770140b641ec9ebae971e8114f46f308b4b
Suraj Shetty <surajshetty3416@gmail.com>
2018-07-14 20:11:31 +0530

    Add dropdown component

---
+++ ui/components/Dropdown.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dropdown show">
+      <a class="btn btn-sm btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        {{label}}
+      </a>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+        <a class="dropdown-item" v-for="option in options" :key="option.label" @click="option.handler">{{option.label}}</a>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['label', 'options']
+}
+</script>
+<style>
+
+</style>
+
