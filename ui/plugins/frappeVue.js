46e3fa9193ac0a27d47be4dba5a37db77decb18f
Faris Ansari <netchamp.faris@gmail.com>
2018-07-14 13:01:37 +0530

    Move frappe component initialization into a plugin

---
+++ ui/plugins/frappeVue.js
@@ -0,0 +1,38 @@
+/**
+ * Vue Plugin that registers common UI elements
+ * like Button and Modal into the root Vue instance
+ */
+
+import frappe from 'frappejs';
+import NotFound from '../components/NotFound';
+import FeatherIcon from '../components/FeatherIcon';
+import FrappeControl from '../components/controls/FrappeControl';
+import Button from '../components/Button';
+import Indicator from '../components/Indicator';
+import modalPlugin from '../components/Modal/plugin';
+import formModalPlugin from '../plugins/formModal';
+
+export default function installFrappePlugin(Vue) {
+  Vue.component('not-found', NotFound);
+  Vue.component('feather-icon', FeatherIcon);
+  Vue.component('frappe-control', FrappeControl);
+  Vue.component('f-button', Button);
+  Vue.component('indicator', Indicator);
+
+  Vue.use(modalPlugin);
+  Vue.use(formModalPlugin);
+
+  Vue.mixin({
+    computed: {
+      frappe() {
+        return frappe;
+      }
+    },
+    methods: {
+      // global translation function in every component
+      _(...args) {
+        return frappe._(...args);
+      }
+    }
+  });
+}
