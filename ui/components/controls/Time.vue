5ebba28af485f2020e6c8041133a5dc3729e8fca
Faris Ansari <netchamp.faris@gmail.com>
2018-07-14 12:46:40 +0530

    Commonify Date and Time component into BaseDate component

--- ui/components/controls/Time.vue
+++ ui/components/controls/Time.vue
@@ -1,39 +1,16 @@
-<template>
-  <div :class="{ 'form-group': !onlyInput }">
-    <label v-if="!onlyInput">{{ docfield.label }}</label>
-    <flat-pickr
-      :value="value"
-      :config="config"
-      class="form-control"
-      @on-change="emitChange"
-    >
-    </flat-pickr>
-  </div>
-</template>
 <script>
-import flatPickr from 'vue-flatpickr-component';
-import Data from './Data';
+import BaseDate from './BaseDate';
 
 export default {
-  extends: Data,
-  data() {
-    return {
-      config: {
+  extends: BaseDate,
+  props: {
+    config: {
+      type: Object,
+      default: () => ({
         enableTime: true,
         noCalendar: true
-      }
-    };
-  },
-  components: {
-    flatPickr
-  },
-  methods: {
-    emitChange(times, timeString) {
-      this.$emit('change', timeString);
+      })
     }
   }
 };
 </script>
-<style lang="scss">
-@import "flatpickr/dist/flatpickr.css";
-</style>
