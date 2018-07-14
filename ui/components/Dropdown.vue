b9fd7b9ddd179af640c06a7d5c9813b6cde764e9
Suraj Shetty <surajshetty3416@gmail.com>
2018-07-14 20:31:44 +0530

    Format Dropdown component

--- ui/components/Dropdown.vue
+++ ui/components/Dropdown.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="dropdown show">
-      <a class="btn btn-sm btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <a class="btn btn-sm btn-secondary dropdown-toggle"
+        href="#"
+        role="button"
+        id="dropdownMenuLink"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false">
         {{label}}
       </a>
       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-        <a class="dropdown-item" v-for="option in options" :key="option.label" @click="option.handler">{{option.label}}</a>
+        <a class="dropdown-item"
+          v-for="option in options"
+          :key="option.label"
+          @click="option.handler">
+          {{option.label}}
+        </a>
       </div>
   </div>
 </template>
