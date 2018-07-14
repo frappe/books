d8d1eb69a2f2504d32f36c6e2c9694f9394f24c2
Faris Ansari <netchamp.faris@gmail.com>
2018-07-14 21:42:34 +0530

    Rename number_format => numberFormat

--- common/index.js
+++ common/index.js
@@ -1,5 +1,5 @@
 const utils = require('../utils');
-const number_format = require('../utils/number_format');
+const numberFormat = require('../utils/numberFormat');
 const format = require('../utils/format');
 const errors = require('./errors');
 const BaseDocument = require('frappejs/model/document');
@@ -8,7 +8,7 @@ const BaseMeta = require('frappejs/model/meta');
 module.exports = {
     initLibs(frappe) {
         Object.assign(frappe, utils);
-        Object.assign(frappe, number_format);
+        Object.assign(frappe, numberFormat);
         Object.assign(frappe, format);
         frappe.errors = errors;
         frappe.BaseDocument = BaseDocument;
