fd40c193a1841f1cb32ed3536e201d645fb04f57
Faris Ansari <netchamp.faris@gmail.com>
2018-07-15 13:28:24 +0530

    Add a loading page in SetupWizard

--- src/pages/SetupWizard/index.vue
+++ src/pages/SetupWizard/index.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="setup-wizard d-flex justify-content-center">
     <div class="col-4 border rounded shadow-sm p-4 mt-5">
-      <h4 class="text-center">Setup your account</h4>
-      <div class="progress-indicator d-flex justify-content-center p-4">
-        <indicator class="mr-1" :color="indicatorColor(index)" v-for="(section, index) in layout.sections" :key="index" />
+      <div v-if="showForm" key="setup-wizard-form">
+        <h4 class="text-center">Setup your account</h4>
+        <div class="progress-indicator d-flex justify-content-center p-4">
+          <indicator class="mr-1" :color="indicatorColor(index)" v-for="(section, index) in layout.sections" :key="index" />
+        </div>
+        <form-layout
+          :doc="doc"
+          :fields="fields"
+          :layout="layout"
+          :currentSection="currentSection"
+        />
+        <div class="d-flex justify-content-between">
+          <div>
+            <f-button secondary v-if="currentSection > 0"
+              @click="prevSection">
+              Prev
+            </f-button>
+          </div>
+          <div>
+            <f-button primary v-if="currentSection < layout.sections.length - 1"
+              @click="nextSection">
+              Next
+            </f-button>
+            <f-button primary v-if="currentSection === layout.sections.length - 1"
+              @click="submit">
+              Complete
+            </f-button>
+          </div>
+        </div>
       </div>
-      <form-layout
-        :doc="doc"
-        :fields="fields"
-        :layout="layout"
-        :currentSection="currentSection"
-      />
-      <div class="d-flex justify-content-between">
-        <div>
-          <f-button secondary v-if="currentSection > 0"
-            @click="prevSection">
-            Prev
-          </f-button>
-        </div>
-        <div>
-          <f-button primary v-if="currentSection < layout.sections.length - 1"
-            @click="nextSection">
-            Next
-          </f-button>
-          <f-button primary v-if="currentSection === layout.sections.length - 1"
-            @click="submit">
-            Complete
-          </f-button>
-        </div>
+      <div class="d-flex flex-column justify-content-center align-items-center" v-else key="setup-wizard-loading-screen">
+        <feather-icon class="loading-icon" name="loader" />
+        <span class="text-muted text-center">
+          {{ _('Hang tight.') }}<br>{{ _('We are setting up your account.') }}
+        </span>
       </div>
     </div>
   </div>
@@ -43,7 +51,8 @@ export default {
   name: 'SetupWizard',
   data() {
     return {
-      currentSection: 0
+      currentSection: 0,
+      showForm: true
     };
   },
   components: {
@@ -54,6 +63,7 @@ export default {
   },
   methods: {
     async submit() {
+      this.showForm = false;
       this.$emit('complete', this.doc);
     },
     nextSection() {
@@ -78,3 +88,19 @@ export default {
   }
 };
 </script>
+<style scoped>
+.loading-icon {
+  height: 24px;
+  animation: rotation 1s infinite linear;
+  transform-origin: 50% 50%;
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+</style>
