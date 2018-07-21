29f6f2096800de592e0ab038f70be575970d6eb3
inishchith <inishchith@gmail.com>
2018-07-21 12:59:29 +0530

    remove crap

--- email/getConfig.js
+++
@@ -1,8 +0,0 @@
-const frappe = require('frappejs');
-module.exports = async function getData(emailAddress) {
-    account = await frappe.db.getAll({
-        doctype: 'EmailAccount',
-        fields: ['*']
-    })
-    return account;
-}
