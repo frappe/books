2614ff82300699e005a869af9a1968ceb10b9764
sahil28297 <37302950+sahil28297@users.noreply.github.com>
2018-10-10 15:24:33 +0530

    Removed Redundant files from client folder (#99)
    * Removed redundant files from client folder
    
    * removed console statement

--- client/view/controls/dynamicLink.js
+++
@@ -1,9 +0,0 @@
-const LinkControl = require('./link');
-
-class DynamicLinkControl extends LinkControl {
-    getTarget() {
-        return this.doc[this.references];
-    }
-};
-
-module.exports = DynamicLinkControl;
