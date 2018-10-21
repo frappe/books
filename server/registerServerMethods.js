dfbddd651a8c1e1dd18cc5469a3c8736ebd9b424
Faris Ansari <netchamp.faris@gmail.com>
2018-10-21 18:19:33 +0530

    Initialize db in electron entry

---
+++ server/registerServerMethods.js
@@ -0,0 +1,15 @@
+const frappe = require('frappejs');
+const registerReportMethods = require('../reports');
+
+module.exports = function registerServerMethods() {
+  registerReportMethods();
+
+  frappe.registerMethod({
+    method: 'import-coa',
+    async handler() {
+      const standardCOA = require('../fixtures/standardCOA');
+      const importCOA = require('../models/doctype/Account/importCOA');
+      await importCOA(standardCOA);
+    }
+  });
+}
--- src/main-electron.js
+++ src/main-electron.js
@@ -6,46 +6,48 @@ import frappeVue from 'frappejs/ui/plugins/frappeVue';
 
 // frappejs imports
 import frappe from 'frappejs';
-import frappeConf from '../frappe.conf';
-import HTTPClient from 'frappejs/backends/http';
 import SQLite from 'frappejs/backends/sqlite';
 import common from 'frappejs/common';
 import coreModels from 'frappejs/models';
 import models from '../models';
-import registerReportMethods from '../reports';
+import registerServerMethods from '../server/registerServerMethods';
 
-console.log('electron is here')
+(async () => {
+  frappe.isServer = true;
+  frappe.init();
+  frappe.registerLibs(common);
+  frappe.registerModels(coreModels);
+  frappe.registerModels(models);
+  frappe.fetch = window.fetch.bind();
+  frappe.login('Administrator');
+  frappe.db = new SQLite({ dbPath: 'test.db' });
+  await frappe.db.connect();
+  await frappe.db.migrate();
 
-frappe.init();
-frappe.registerLibs(common);
-frappe.registerModels(coreModels);
-frappe.registerModels(models);
-frappe.fetch = window.fetch.bind();
-frappe.db = new SQLite({ dbPath: 'test.db' });
+  frappe.getSingle('SystemSettings');
+  registerServerMethods();
 
-frappe.getSingle('SystemSettings');
-registerReportMethods();
+  frappe.getSingle('AccountingSettings')
+    .then(accountingSettings => {
+      if (router.currentRoute.fullPath !== '/') return;
 
-frappe.getSingle('AccountingSettings')
-  .then(accountingSettings => {
-    if (router.currentRoute.fullPath !== '/') return;
+      if (accountingSettings.companyName) {
+        router.push('/tree/Account');
+      } else {
+        router.push('/setup-wizard');
+      }
+    });
 
-    if (accountingSettings.companyName) {
-      router.push('/tree/Account');
-    } else {
-      router.push('/setup-wizard');
-    }
+  window.frappe = frappe;
+
+  Vue.config.productionTip = false;
+  Vue.use(frappeVue);
+
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
   });
-
-window.frappe = frappe;
-
-Vue.config.productionTip = false;
-Vue.use(frappeVue);
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-});
+})()
