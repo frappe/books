cc28e0ee45c7238e4718a601ff716aabf3a04c46
Faris Ansari <netchamp.faris@gmail.com>
2018-10-26 02:26:35 +0530

    Add payment list

---
+++ models/doctype/Payment/PaymentList.js
@@ -0,0 +1,13 @@
+import { _ } from 'frappejs/utils';
+
+export default {
+  doctype: 'Payment',
+  title: _('Payment'),
+  columns: [
+    'party',
+    'account',
+    'amount',
+    'date',
+    'name'
+  ]
+}
