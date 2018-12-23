378be8d4392565dbae1a6716ae094939a37db802
Jaffrey Joy <jaffreyjoy@gmail.com>
2018-12-23 18:26:37 +0530

    [feat] add Export Wizard component for customizing export

--- src/pages/Report/index.vue
+++ src/pages/Report/index.vue
@@ -3,7 +3,7 @@
         <div class="p-4">
             <h4 class="pb-2">{{ reportConfig.title }}</h4>
             <div class="col-12 text-right mt-4 mb-4">
-              <f-button primary @click="exportData">{{ 'Export' }}</f-button>
+              <f-button primary @click="openExportWizard">{{ 'Export' }}</f-button>
             </div>
             <report-filters v-if="filtersExists" :filters="reportConfig.filterFields" :filterDefaults="filters" @change="getReportData"></report-filters>
             <div class="pt-2" ref="datatable" v-once></div>
@@ -16,8 +16,8 @@ import DataTable from 'frappe-datatable';
 import frappe from 'frappejs';
 import ReportFilters from 'frappejs/ui/pages/Report/ReportFilters';
 import utils from 'frappejs/client/ui/utils';
-import path from 'path';
-const { writeFile } = require('frappejs/server/utils');
+import ExportWizard from '../../components/ExportWizard';
+
 
 export default {
   name: 'Report',
@@ -28,33 +28,30 @@ export default {
     }
   },
   methods: {
-    async exportData() {
+    async openExportWizard() {
+      this.$modal.show({
+        modalProps: {
+          title: 'Export Wizard',
+          noFooter: true
+        },
+        component: ExportWizard,
+        props: await this.getColumnNames()
+      });
+    },
+    async getColumnNames() {
       let { title, filterFields } = this.reportConfig;
-      console.log(title, filterFields);
       let [rows, columns] = await this.getReportData(filterFields || []);
-      let columnNames = columns.map((column)=>column.content).toString();
-      let columnIDs = columns.map((column)=>column.id);
-      let rowData = rows.map((row)=>columnIDs.map((id)=>row[id]).toString());
-      let csvDataArray = [columnNames, ...rowData];
-      let csvData = csvDataArray.join('\n');
-      // console.log(csvData);
-      let d = new Date();
-      if(localStorage.dbPath) {
-        let fileName = [
-          title.replace(/\s/g,'-'),
-          [d.getDate(),d.getMonth(),d.getFullYear()].join('-'),
-          `${d.getTime()}.csv`
-        ].join('_');
-        let dbPath = localStorage.dbPath;
-        let dbFilePath = dbPath.substring(0,
-          dbPath.length - dbPath.split('').reverse().join('').indexOf('/') - 1
-        )
-        // console.log(dbFilePath);
-        let filePath = path.join(dbFilePath, fileName);
-        await writeFile(filePath, csvData);
-        alert(`CSV File saved at ${filePath}`);
-      } else {
-        alert('DB Filepath not set!');
+      let columnData = columns.map((column)=>{
+        return {
+          id: column.id,
+          content: column.content,
+          checked: true
+      }});
+      // console.log(columnData);
+      return {
+        title: title,
+        rows: rows,
+        columnData: columnData
       }
     },
     async getReportData(filters) {
@@ -85,9 +82,6 @@ export default {
       for(let column of columns) {
          column.editable = false;
       }
-
-      console.log(rows);
-      console.log(columns);
 
       if (this.datatable) {
         this.datatable.refresh(rows, columns);
