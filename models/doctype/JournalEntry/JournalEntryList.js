394c6059491ab343648ca6f8276596429318caea
thefalconx33 <thefalconx33@gmail.com>
2019-07-16 14:44:44 +0530

    Sidebar with Groups

---
+++ models/doctype/JournalEntry/JournalEntryList.js
@@ -0,0 +1,7 @@
+import { _ } from 'frappejs/utils';
+
+export default {
+  doctype: 'JournalEntry',
+  title: _('Journal Entry'),
+  columns: ['date', 'entryType']
+};
--- src/pages/ListView/listConfig.js
+++ src/pages/ListView/listConfig.js
@@ -3,11 +3,13 @@ import Customer from '../../../models/doctype/Party/CustomerList';
 import Item from '../../../models/doctype/Item/ItemList';
 import Payment from '../../../models/doctype/Payment/PaymentList';
 import Tax from '../../../models/doctype/Tax/TaxList';
+import JournalEntry from '../../../models/doctype/JournalEntry/JournalEntryList';
 
 export default {
   Invoice,
   Customer,
   Item,
   Payment,
-  Tax
-}
+  Tax,
+  JournalEntry
+};
