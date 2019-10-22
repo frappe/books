2cac5f1a8c6c2e8fec6ac71e13d8777293280a79
Faris Ansari <netchamp.faris@gmail.com>
2019-10-22 18:53:08 +0530

    fix: Window draggable from PageHeader

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-4 px-8 flex justify-between items-center">
+  <div class="mt-4 px-8 flex justify-between items-center window-drag">
     <slot name="title" />
     <div class="flex items-center">
       <slot name="actions" />
