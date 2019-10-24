24869578c72d064fab53c847232ef8ee217f5b4f
Faris Ansari <netchamp.faris@gmail.com>
2019-10-24 16:09:18 +0530

    fix: Select icon

--- src/components/Controls/Select.vue
+++ src/components/Controls/Select.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
+  <div class="relative flex items-center justify-end bg-white focus-within:bg-gray-200" :class="inputClasses">
     <select
-      class="appearance-none bg-white"
-      :class="inputClasses"
+      class="appearance-none bg-transparent focus:outline-none w-full"
       :value="value"
       @blur="e => triggerChange(e.target.value)"
     >
       <option v-for="option in options" :value="option.value">{{ option.label }}</option>
     </select>
+    <svg class="w-3 h-3 absolute" viewBox="0 0 5 10" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 2.636L2.636 1l1.637 1.636M1 7.364L2.636 9l1.637-1.636"
+        stroke="#404040"
+        fill="none"
+        fill-rule="evenodd"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
   </div>
 </template>
 
