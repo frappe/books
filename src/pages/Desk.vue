df2d292c781ed258cb1ef4f1d0447f75f6cae49b
Faris Ansari <netchamp.faris@gmail.com>
2019-11-08 16:16:24 +0530

    fix: move logic to computed

--- src/pages/Desk.vue
+++ src/pages/Desk.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="flex">
-    <Sidebar class="w-56" />
+    <Sidebar class="w-56 flex-shrink-0" />
     <div class="flex flex-1 overflow-y-hidden bg-white">
       <keep-alive include="InvoiceForm">
         <router-view class="flex-1" :key="$route.fullPath" />
       </keep-alive>
-      <div class="flex" v-if="$route.query.edit && $route.query.doctype && $route.query.name">
+      <div class="flex" v-if="showQuickEdit">
         <keep-alive>
-          <router-view name="edit" class="w-80 flex-1" :key="$route.query.doctype + $route.query.name"/>
+          <router-view
+            name="edit"
+            class="w-80 flex-1"
+            :key="$route.query.doctype + $route.query.name"
+          />
         </keep-alive>
       </div>
     </div>
@@ -19,6 +23,15 @@ export default {
   name: 'Desk',
   components: {
     Sidebar
+  },
+  computed: {
+    showQuickEdit() {
+      return (
+        this.$route.query.edit &&
+        this.$route.query.doctype &&
+        this.$route.query.name
+      );
+    }
   }
 };
 </script>
