9120942f69cb1d9b03ccda85511426bccaaa8fc2
Faris Ansari <netchamp.faris@gmail.com>
2019-11-19 23:25:28 +0530

    feat: Check and DynamicLink control

---
+++ src/components/Controls/DynamicLink.vue
@@ -0,0 +1,26 @@
+<script>
+import Link from './Link';
+export default {
+  name: 'DynamicLink',
+  props: ['target'],
+  extends: Link,
+  created() {
+    this.targetWatcher = this.$watch(`doc.${this.df.references}`, function(newTarget, oldTarget) {
+      if (newTarget !== oldTarget) {
+        this.triggerChange('');
+      }
+    });
+  },
+  destroyed() {
+    this.targetWatcher();
+  },
+  methods: {
+    getTarget() {
+      if (!this.doc) {
+        throw new Error('You must provide `doc` for DynamicLink to work.');
+      }
+      return this.doc[this.df.references];
+    }
+  }
+};
+</script>
