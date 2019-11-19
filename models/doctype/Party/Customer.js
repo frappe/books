b2c5eeb61841cd7cb5b3fb64149cebf9ca292fce
Faris Ansari <netchamp.faris@gmail.com>
2019-11-20 00:46:16 +0530

    fix: Create Invoice action from Customer

--- models/doctype/Party/Customer.js
+++ models/doctype/Party/Customer.js
@@ -1,8 +1,32 @@
+const { _ } = require('frappejs/utils');
+const router = require('@/router').default;
+const frappe = require('frappejs');
+
 module.exports = {
   name: 'Customer',
   label: 'Customer',
   basedOn: 'Party',
   filters: {
     customer: 1
-  }
+  },
+  actions: [
+    {
+      label: _('Create Invoice'),
+      action: async customer => {
+        let doc = await frappe.getNewDoc('SalesInvoice');
+        router.push({
+          path: `/edit/SalesInvoice/${doc.name}`,
+          query: {
+            values: {
+              customer: customer.name
+            }
+          }
+        });
+      }
+    },
+    // {
+    //   label: _('View Invoices'),
+    //   action: console.log
+    // }
+  ]
 };
