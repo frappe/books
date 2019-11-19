0a3900c3f43961f022b95c4207322a3322149fc5
Faris Ansari <netchamp.faris@gmail.com>
2019-11-19 23:20:33 +0530

    fix: Use TwoColumnForm in SetupWizard

--- src/pages/SetupWizard/SetupWizard.vue
+++ src/pages/SetupWizard/SetupWizard.vue
@@ -1,27 +1,11 @@
 <template>
-  <div class="py-10 flex-1 bg-white">
+  <div class="py-10 flex-1 bg-white window-drag">
     <div class="px-12">
       <h1 class="text-2xl font-semibold">{{ _('Setup your organization') }}</h1>
       <p class="text-gray-600">{{ _('These settings can be changed later') }}</p>
     </div>
     <div class="px-8 mt-5">
-      <div class="border-t">
-        <div
-          class="grid border-b text-xs"
-          style="grid-template-columns: 1fr 2fr"
-          v-for="df in fields"
-        >
-          <div class="py-2 pl-4 text-gray-600 flex items-center">{{ df.label }}</div>
-          <div class="py-2 pr-4">
-            <FormControl
-              size="small"
-              :df="df"
-              :value="doc[df.fieldname]"
-              @change="value => doc.set(df.fieldname, value)"
-            />
-          </div>
-        </div>
-      </div>
+      <TwoColumnForm :fields="fields" :doc="doc" />
     </div>
     <div class="px-8 flex justify-end mt-5">
       <Button @click="submit" type="primary" class="text-white text-sm">{{ _('Next') }}</Button>
@@ -30,7 +14,7 @@
 </template>
 <script>
 import frappe from 'frappejs';
-import FormControl from '@/components/Controls/FormControl';
+import TwoColumnForm from '@/components/TwoColumnForm';
 import Button from '@/components/Button';
 
 export default {
@@ -48,7 +32,7 @@ export default {
     };
   },
   components: {
-    FormControl,
+    TwoColumnForm,
     Button
   },
   async beforeMount() {
