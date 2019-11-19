f975446801fa857e8a3480594683cfe045c5af58
Faris Ansari <netchamp.faris@gmail.com>
2019-11-20 01:59:25 +0530

    fix: Groups in Dropdown

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative" v-on-outside-click="() => isShown = false">
+  <div class="relative" v-on-outside-click="() => (isShown = false)">
     <div class="h-full">
       <slot
         :toggleDropdown="toggleDropdown"
@@ -14,31 +14,44 @@
       v-if="isShown"
     >
       <div class="p-1 max-h-64 overflow-auto">
-        <a
-          ref="items"
-          class="block p-2 rounded mt-1 first:mt-0 cursor-pointer whitespace-no-wrap"
-          v-for="(d, index) in items"
-          :key="d.label"
-          :class="index === highlightedIndex ? 'bg-gray-100' : ''"
-          @mouseenter="highlightedIndex = index"
-          @mouseleave="highlightedIndex = -1"
-          @click="selectItem(d)"
-        >
-          <component :is="d.component" v-if="d.component" />
-          <template v-else>{{ d.label }}</template>
-        </a>
+        <div v-for="d in dropdownItems" :key="d.label">
+          <div
+            v-if="d.isGroup"
+            class="px-2 pt-3 pb-1 text-xs uppercase text-gray-700 font-semibold tracking-wider"
+          >
+            {{ d.label }}
+          </div>
+          <a
+            v-else
+            ref="items"
+            class="block p-2 rounded mt-1 first:mt-0 cursor-pointer whitespace-no-wrap"
+            :class="d.index === highlightedIndex ? 'bg-gray-100' : ''"
+            @mouseenter="highlightedIndex = d.index"
+            @mouseleave="highlightedIndex = -1"
+            @click="selectItem(d)"
+          >
+            <component :is="d.component" v-if="d.component" />
+            <template v-else>{{ d.label }}</template>
+          </a>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import uniq from 'lodash/uniq';
+
 export default {
   name: 'Dropdown',
   props: {
     items: {
       type: Array,
       default: () => []
+    },
+    groups: {
+      type: Array,
+      default: null
     },
     right: {
       type: Boolean,
@@ -50,6 +63,59 @@ export default {
       isShown: false,
       highlightedIndex: -1
     };
+  },
+  computed: {
+    sortedGroups() {
+      if (Array.isArray(this.groups)) {
+        return this.groups;
+      }
+      let groupNames = uniq(
+        this.items
+          .map(d => d.group)
+          .filter(Boolean)
+          .sort()
+      );
+      if (groupNames.length > 0) {
+        return groupNames;
+      }
+      return null;
+    },
+    dropdownItems() {
+      if (this.sortedGroups) {
+        let itemsByGroup = {};
+
+        for (let item of this.items) {
+          let group = item.group || '';
+          itemsByGroup[group] = itemsByGroup[group] || [];
+          itemsByGroup[group].push(item);
+        }
+
+        let items = [];
+        let i = 0;
+        for (let group of this.sortedGroups) {
+          let groupItems = itemsByGroup[group];
+          groupItems = groupItems.map(d => {
+            d.index = i;
+            i++;
+            return d;
+          });
+          items = items.concat(
+            {
+              label: group,
+              isGroup: true
+            },
+            groupItems
+          );
+        }
+
+        return items;
+      }
+
+      return this.items.map((d, i) => {
+        d.index = i;
+        return d;
+      });
+    }
   },
   methods: {
     selectItem(d) {
