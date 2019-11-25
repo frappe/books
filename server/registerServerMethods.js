387254fbbb070e0b38acc4e9ad35a799a9acc3b5
Faris Ansari <netchamp.faris@gmail.com>
2019-11-25 23:59:53 +0530

    fix: Remove unused imports and Vue plugins

--- server/registerServerMethods.js
+++ server/registerServerMethods.js
@@ -1,42 +1,14 @@
 const frappe = require('frappejs');
 const registerReportMethods = require('../reports');
-const sender = require('../email/sender');
 
 module.exports = function registerServerMethods() {
   registerReportMethods();
-
-  frappe.registerMethod({
-    method: 'send-mail',
-    handler: sender.sendMail
-  });
 
   frappe.registerMethod({
     method: 'import-coa',
     async handler() {
       const importCOA = require('../models/doctype/Account/importCOA');
       await importCOA();
-    }
-  });
-
-  frappe.registerMethod({
-    method: 'print-pdf',
-    handler({ doctype, name, html }) {
-      if (frappe.isElectron) {
-        const path = require('path');
-        const { getPDFForElectron } = require('frappejs/server/pdf');
-        const { getSettings } = require('../electron/settings');
-        getPDFForElectron(doctype, name, undefined, html);
-      }
-    }
-  });
-  frappe.registerMethod({
-    method: 'show-dialog',
-    handler({ title, message }) {
-      frappe.showModal({
-        modalProps: { title, noFooter: true },
-        component: require('../src/components/MessageDialog').default,
-        props: { message }
-      });
     }
   });
 };
