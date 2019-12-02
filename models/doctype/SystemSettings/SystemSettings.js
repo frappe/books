2f727ebd0c3c7e64a77863bbda8285d0936b78af
Faris Ansari <netchamp.faris@gmail.com>
2019-12-02 17:47:54 +0530

    fix: Add floatPrecision field

--- models/doctype/SystemSettings/SystemSettings.js
+++ models/doctype/SystemSettings/SystemSettings.js
@@ -1,26 +1,34 @@
 module.exports = {
-    name: "SystemSettings",
-    label: "System Settings",
-    doctype: "DocType",
-    isSingle: 1,
-    isChild: 0,
-    keywordFields: [],
-    fields: [
-        {
-            fieldname: "dateFormat",
-            label: "Date Format",
-            fieldtype: "Select",
-            options: [
-                "dd/MM/yyyy",
-                "MM/dd/yyyy",
-                "dd-MM-yyyy",
-                "MM-dd-yyyy",
-                "yyyy-MM-dd",
-                "d MMM, y",
-                "MMM d, y"
-            ],
-            default: "MMM d, y",
-            required: 1
-        }
-    ]
-}
+  name: 'SystemSettings',
+  label: 'System Settings',
+  doctype: 'DocType',
+  isSingle: 1,
+  isChild: 0,
+  keywordFields: [],
+  fields: [
+    {
+      fieldname: 'dateFormat',
+      label: 'Date Format',
+      fieldtype: 'Select',
+      options: [
+        'dd/MM/yyyy',
+        'MM/dd/yyyy',
+        'dd-MM-yyyy',
+        'MM-dd-yyyy',
+        'yyyy-MM-dd',
+        'd MMM, y',
+        'MMM d, y'
+      ],
+      default: 'MMM d, y',
+      required: 1
+    },
+    {
+      fieldname: 'floatPrecision',
+      label: 'Float Precision',
+      fieldtype: 'Select',
+      options: ['2', '3', '4', '5'],
+      default: '2',
+      required: 1
+    },
+  ]
+};
