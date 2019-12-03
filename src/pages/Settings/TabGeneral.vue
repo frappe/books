9b8094ab7963189bfc05ef07a83a3b663c0cf277
Faris Ansari <netchamp.faris@gmail.com>
2019-12-03 13:51:10 +0530

    fix(Settings): Add writeOffAccount and roundOffAccount

--- src/pages/Settings/TabGeneral.vue
+++ src/pages/Settings/TabGeneral.vue
@@ -49,6 +49,8 @@ export default {
         'country',
         'bankName',
         'currency',
+        'writeOffAccount',
+        'roundOffAccount',
         'fiscalYearStart',
         'fiscalYearEnd'
       ].map(fieldname => meta.getField(fieldname));
