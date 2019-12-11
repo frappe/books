df6ace3b8d6837be0ec82b2ff6c5fa57e1fed5ba
Faris Ansari <netchamp.faris@gmail.com>
2019-12-11 14:34:27 +0530

    feat: DatePicker

--- src/components/Controls/Date.vue
+++ src/components/Controls/Date.vue
@@ -1,12 +1,39 @@
+<template>
+  <div>
+    <div class="text-gray-600 text-sm mb-1" v-if="showLabel">
+      {{ df.label }}
+    </div>
+    <DatePicker
+      ref="input"
+      :class="[inputClasses, 'cursor-text']"
+      :value="value"
+      :placeholder="inputPlaceholder"
+      :readonly="isReadOnly"
+      :format-value="formatValue"
+      @change="value => triggerChange(value)"
+    />
+  </div>
+</template>
+
 <script>
+import frappe from 'frappejs';
 import Base from './Base';
+import DatePicker from '../DatePicker/DatePicker';
 
 export default {
   name: 'Date',
   extends: Base,
+  components: {
+    DatePicker
+  },
   computed: {
     inputType() {
       return 'date';
+    }
+  },
+  methods: {
+    formatValue(value) {
+      return frappe.format(value, this.df);
     }
   }
 };
