bd5ee080c23aba9ad305d6a2f66e916fb664b738
Faris Ansari <netchamp.faris@gmail.com>
2019-12-18 23:54:58 +0530

    fix: Formatting and model name

--- models/doctype/SalesOrder/SalesOrder.js
+++ models/doctype/SalesOrder/SalesOrder.js
@@ -2,13 +2,13 @@ const model = require('frappejs/model');
 const Quotation = require('../Quotation/Quotation');
 
 module.exports = model.extend(Quotation, {
-    name: "SalesOrder",
-    label: "Sales Order",
-    settings: "SalesOrderSettings",
-    fields: [
-        {
-            "fieldname": "items",
-            "childtype": "SalesOrderItem"
-        }
-    ]
+  name: 'SalesOrder',
+  label: 'Sales Order',
+  settings: 'SalesOrderSettings',
+  fields: [
+    {
+      fieldname: 'items',
+      childtype: 'SalesOrderItem'
+    }
+  ]
 });
