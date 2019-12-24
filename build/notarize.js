ed2cbaae06d72ecf911e0b33b8ab7f6fafa6e09a
Faris Ansari <netchamp.faris@gmail.com>
2019-12-24 15:44:28 +0530

    chore: Add config for signing and notarize

---
+++ build/notarize.js
@@ -0,0 +1,17 @@
+const { notarize } = require('electron-notarize');
+
+exports.default = async context => {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'io.frappe.accounting',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_PASSWORD
+  });
+};
