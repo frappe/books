de19af85c4db1a98386ef420125b83f93b7a5b21
Faris Ansari <netchamp.faris@gmail.com>
2019-12-26 23:38:17 +0530

    fix: Incorrect accountType bug
    - Set accountType as non mandatory
    - accountType can be set from parent, but it is not required

--- models/doctype/Account/AccountDocument.js
+++ models/doctype/Account/AccountDocument.js
@@ -2,13 +2,13 @@ const frappe = require('frappejs');
 const BaseDocument = require('frappejs/model/document');
 
 module.exports = class Account extends BaseDocument {
-    async validate() {
-        if (!this.accountType) {
-            if (this.parentAccount) {
-                this.accountType = await frappe.db.getValue('Account', this.parentAccount, 'accountType');
-            } else {
-                this.accountType = 'Asset';
-            }
-        }
+  async validate() {
+    if (!this.accountType && this.parentAccount) {
+      this.accountType = await frappe.db.getValue(
+        'Account',
+        this.parentAccount,
+        'accountType'
+      );
     }
-}
+  }
+};
