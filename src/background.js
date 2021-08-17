6a2644a4a115d33e43e5d19241088474092c6639
Ankit Singhaniya <ankit.singhaniyaz@gmail.com>
2021-08-18 02:07:52 +0530

    fix building

--- src/background.js
+++ src/background.js
@@ -4,7 +4,7 @@ import { app, protocol, BrowserWindow, ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import {
   createProtocol,
-  installVueDevtools
+  // installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib';
 import theme from '@/theme';
 import { getMainWindowSize } from './screenSize';
@@ -118,11 +118,11 @@ app.on('ready', async () => {
     // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
     // If you are not using Windows 10 dark mode, you may uncomment these lines
     // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
-    try {
-      await installVueDevtools();
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString());
-    }
+    // try {
+    //   await installVueDevtools();
+    // } catch (e) {
+    //   console.error('Vue Devtools failed to install:', e.toString());
+    // }
   }
   createWindow();
 });
