b8671cb2ed91063f39d6bd98e8b342e6de5d6f06
18alantom <2.alan.tom@gmail.com>
2021-11-25 14:49:50 +0530

    fix: multiple payment related fixes - outstanding amount reset on payment cancel - payment reference type is now select - map property on select fields for different display values - multiple payment validations - sensible binding payment amount and reference amount

--- models/doctype/PaymentFor/PaymentFor.js
+++ models/doctype/PaymentFor/PaymentFor.js
@@ -4,34 +4,40 @@ export default {
   isSingle: 0,
   isChild: 1,
   keywordFields: [],
-  tableFields: [
-    'referenceType',
-    'referenceName',
-    'amount'
-  ],
+  tableFields: ['referenceType', 'referenceName', 'amount'],
   fields: [
     {
       fieldname: 'referenceType',
       label: 'Reference Type',
-      fieldtype: 'AutoComplete',
+      fieldtype: 'Select',
       options: ['SalesInvoice', 'PurchaseInvoice'],
-      required: 1
+      map: { SalesInvoice: 'Invoice', PurchaseInvoice: 'Bill' },
+      required: 1,
     },
     {
       fieldname: 'referenceName',
       label: 'Reference Name',
       fieldtype: 'DynamicLink',
       references: 'referenceType',
-      required: 1
+      getFilters() {
+        return {
+          outstandingAmount: ['>', 0],
+        };
+      },
+      required: 1,
     },
     {
       fieldname: 'amount',
       label: 'Amount',
       fieldtype: 'Currency',
       formula: (row, doc) => {
-        return doc.getFrom(row.referenceType, row.referenceName, 'outstandingAmount');
+        return doc.getFrom(
+          row.referenceType,
+          row.referenceName,
+          'outstandingAmount'
+        );
       },
-      required: 1
-    }
-  ]
+      required: 1,
+    },
+  ],
 };
