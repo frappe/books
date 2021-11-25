12b48debc4b18353f4cc7284915902062f180a65
18alantom <2.alan.tom@gmail.com>
2021-11-25 16:48:26 +0530

    refactor: shift autoupdate to systemsettings

--- src/pages/Settings/TabSystem.vue
+++ src/pages/Settings/TabSystem.vue
@@ -1,31 +1,17 @@
 <template>
   <div>
     <TwoColumnForm v-if="doc" :doc="doc" :fields="fields" :autosave="true" />
-    <div class="mt-6">
-      <FormControl
-        :show-label="true"
-        :df="AccountingSettings.meta.getField('autoUpdate')"
-        @change="(value) => AccountingSettings.update('autoUpdate', value)"
-        :value="AccountingSettings.autoUpdate"
-      />
-      <p class="pl-6 mt-1 text-sm text-gray-600">
-        <!-- prettier-ignore -->
-        {{ _('Automatically check for updates and download them if available. The update will be applied after you restart the app.') }}
-      </p>
-    </div>
   </div>
 </template>
 
 <script>
 import frappe from 'frappejs';
 import TwoColumnForm from '@/components/TwoColumnForm';
-import FormControl from '@/components/Controls/FormControl';
 
 export default {
   name: 'TabSystem',
   components: {
     TwoColumnForm,
-    FormControl,
   },
   data() {
     return {
@@ -40,9 +26,6 @@ export default {
     fields() {
       let meta = frappe.getMeta('SystemSettings');
       return meta.getQuickEditFields();
-    },
-    AccountingSettings() {
-      return frappe.AccountingSettings;
     },
   },
 };
