159359daf860cfb8161103f84d81a1f29d97e24a
Piyush Singhania <piyushsinghania28@gmail.com>
2021-12-12 15:30:50 +0530

    Refactored filter fields

--- reports/GoodsAndServiceTax/GSTR1View.js
+++ reports/GoodsAndServiceTax/GSTR1View.js
@@ -2,21 +2,10 @@ const title = 'GSTR 1';
 import baseConfig from './BaseViewConfig';
 import { DateTime } from 'luxon';
 
-const filterFields = baseConfig.filterFields;
-filterFields.forEach((field) => {
-  if (field.fieldname === 'toDate') {
-    field.default = DateTime.local().toISODate();
-  } 
-  else if(field.fieldname === 'fromDate') {
-    field.default = DateTime.local().minus({months: 3}).toISODate();
-  }
-});
-
-
 export default {
   title: title,
   method: 'gstr-1',
-  filterFields,
+  filterFields: baseConfig.filterFields,
   linkFields: baseConfig.linkFields,
   getColumns: baseConfig.getColumns,
 };
