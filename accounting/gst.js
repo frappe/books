42b8c73b1db3690ad02ef0950d752757bed995b1
Ankit Singhaniya <ankit.singhaniyaz@gmail.com>
2021-12-16 11:47:25 +0530

    make hsn and sac code a regional thing

--- accounting/gst.js
+++ accounting/gst.js
@@ -122,7 +122,7 @@ async function generateB2bData(invoices) {
 
     items.forEach((item) => {
       const itemRecord = {
-        num: item.itemCode || 0,
+        num: item.hsnCode || 0,
         itm_det: {
           txval: item.baseAmount,
           rt: GST[item.tax],
