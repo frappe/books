86382f4487be33228f4fef2e7287d55a5108cb58
18alantom <2.alan.tom@gmail.com>
2021-12-21 14:35:24 +0530

    feat: add Export > JSON

--- reports/commonExporter.js
+++ reports/commonExporter.js
@@ -1,3 +1,4 @@
+import frappe from 'frappejs';
 import fs from 'fs/promises';
 import { getSavePath } from '../src/utils';
 
@@ -5,6 +6,15 @@ function templateToInnerText(innerHTML) {
   const temp = document.createElement('template');
   temp.innerHTML = innerHTML.trim();
   return temp.content.firstChild.innerText;
+}
+
+function deObjectify(value) {
+  if (typeof value !== 'object') return value;
+  if (value === null) return '';
+
+  const innerHTML = value.template;
+  if (!innerHTML) return '';
+  return templateToInnerText(innerHTML);
 }
 
 function csvFormat(value) {
@@ -15,17 +25,12 @@ function csvFormat(value) {
   } else if (typeof value === 'object') {
     const innerHTML = value.template;
     if (!innerHTML) return '';
-    return csvFormat(templateToInnerText(innerHTML));
+    return csvFormat(deObjectify(value));
   }
   return value;
 }
 
-async function exportCsv(reportName, getReportData) {
-  const { rows, columns } = getReportData();
-
-  const { filePath, canceled } = await getSavePath(reportName, 'csv');
-  if (canceled || !filePath) return;
-
+async function exportCsv(rows, columns, filePath) {
   const fieldnames = columns.map(({ fieldname }) => fieldname);
   const labels = columns.map(({ label }) => csvFormat(label));
   const csvRows = [
@@ -35,30 +40,61 @@ async function exportCsv(reportName, getReportData) {
   await fs.writeFile(filePath, csvRows.join('\n'));
 }
 
-async function exportJson(reportName, getReportData) {
-  const { rows, columns } = getReportData();
+async function exportJson(rows, columns, filePath, filters, reportName) {
+  const exportObject = {};
+  const fieldnames = columns.map(({ fieldname }) => fieldname);
 
-  const { filePath, canceled } = await getSavePath(reportName, 'json');
+  exportObject.columns = columns.map(({ fieldname, label }) => ({
+    fieldname,
+    label,
+  }));
+
+  exportObject.rows = rows.map((row) =>
+    fieldnames.reduce((acc, f) => {
+      acc[f] = deObjectify(row[f]);
+      return acc;
+    }, {})
+  );
+
+  exportObject.filters = Object.keys(filters)
+    .filter((name) => filters[name] !== null && filters[name] !== undefined)
+    .reduce((acc, name) => {
+      acc[name] = filters[name];
+      return acc;
+    }, {});
+
+  exportObject.timestamp = new Date().toISOString();
+  exportObject.reportName = reportName;
+  exportObject.softwareName = 'Frappe Books';
+  exportObject.softwareVersion = frappe.store.appVersion;
+
+  await fs.writeFile(filePath, JSON.stringify(exportObject));
+}
+
+async function exportReport(extention, reportName, getReportData) {
+  const { rows, columns, filters } = getReportData();
+
+  const { filePath, canceled } = await getSavePath(reportName, extention);
   if (canceled || !filePath) return;
+
+  switch (extention) {
+    case 'csv':
+      await exportCsv(rows, columns, filePath);
+      return;
+    case 'json':
+      await exportJson(rows, columns, filePath, filters, reportName);
+      return;
+    default:
+      return;
+  }
 }
 
 export default function getCommonExportActions(reportName) {
-  return [
-    {
-      group: 'Export',
-      label: 'CSV',
-      type: 'primary',
-      action: async (getReportData) => {
-        await exportCsv(reportName, getReportData);
-      },
-    },
-    {
-      group: 'Export',
-      label: 'JSON',
-      type: 'primary',
-      action: async (getReportData) => {
-        await exportJson(reportName, getReportData);
-      },
-    },
-  ];
+  return ['csv', 'json'].map((ext) => ({
+    group: 'Export',
+    label: ext.toUpperCase(),
+    type: 'primary',
+    action: async (getReportData) =>
+      await exportReport(ext, reportName, getReportData),
+  }));
 }
