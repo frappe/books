ae46407ea1e3f8dd412e93283b2899a131807814
18alantom <2.alan.tom@gmail.com>
2021-12-28 13:45:19 +0530

    fix: update SalesInvoice

--- models/doctype/SalesInvoiceItem/SalesInvoiceItem.js
+++ models/doctype/SalesInvoiceItem/SalesInvoiceItem.js
@@ -34,7 +34,7 @@ export default {
       label: 'Quantity',
       fieldtype: 'Float',
       required: 1,
-      formula: (row) => row.quantity || 1,
+      default: 1,
     },
     {
       fieldname: 'rate',
@@ -42,9 +42,9 @@ export default {
       fieldtype: 'Currency',
       required: 1,
       formula: async (row, doc) => {
-        const baseRate = (await doc.getFrom('Item', row.item, 'rate')) || 0;
-        const exchangeRate = doc.exchangeRate ?? 1;
-        return baseRate / exchangeRate;
+        const baseRate =
+          (await doc.getFrom('Item', row.item, 'rate')) || frappe.pesa(0);
+        return baseRate.div(doc.exchangeRate);
       },
       getCurrency: (row, doc) => doc.currency,
       formulaDependsOn: ['item'],
@@ -53,7 +53,7 @@ export default {
       fieldname: 'baseRate',
       label: 'Rate (Company Currency)',
       fieldtype: 'Currency',
-      formula: (row, doc) => row.rate * doc.exchangeRate,
+      formula: (row, doc) => row.rate.mul(doc.exchangeRate),
       readOnly: 1,
     },
     {
@@ -78,7 +78,7 @@ export default {
       label: 'Amount',
       fieldtype: 'Currency',
       readOnly: 1,
-      formula: (row) => row.quantity * row.rate,
+      formula: (row) => row.rate.mul(row.quantity),
       getCurrency: (row, doc) => doc.currency,
     },
     {
@@ -86,7 +86,7 @@ export default {
       label: 'Amount (Company Currency)',
       fieldtype: 'Currency',
       readOnly: 1,
-      formula: (row, doc) => row.amount * doc.exchangeRate,
+      formula: (row, doc) => row.amount.mul(doc.exchangeRate),
     },
   ],
 };
