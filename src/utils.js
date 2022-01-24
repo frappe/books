d4675c0e5ac5872ce09f781c83564ed5d2d8d08b
18alantom <2.alan.tom@gmail.com>
2022-01-24 11:42:01 +0530

    refactor: replace all frappe._(" instances

--- src/utils.js
+++ src/utils.js
@@ -174,13 +174,13 @@ export function handleErrorWithDialog(e, doc) {
 
 export async function makePDF(html, savePath) {
   await ipcRenderer.invoke(IPC_ACTIONS.SAVE_HTML_AS_PDF, html, savePath);
-  showExportInFolder(frappe._('Save as PDF Successful'), savePath);
+  showExportInFolder(frappe.t('Save as PDF Successful'), savePath);
 }
 
 export function showExportInFolder(message, filePath) {
   showToast({
     message,
-    actionText: frappe._('Open Folder'),
+    actionText: frappe.t('Open Folder'),
     type: 'success',
     action: async () => {
       await showItemInFolder(filePath);
