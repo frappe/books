7292a30c45abd961b7df3ae5825d804f6da97bc6
18alantom <2.alan.tom@gmail.com>
2022-02-09 12:12:04 +0530

    fix: deprecate earlier translation methods

--- frappe/utils/translation.js
+++ frappe/utils/translation.js
@@ -1,24 +1,5 @@
 import { ValueError } from '../common/errors';
 
-function stringReplace(str, args) {
-  if (!Array.isArray(args)) {
-    args = [args];
-  }
-
-  if (str == undefined) return str;
-
-  let unkeyed_index = 0;
-  return str.replace(/\{(\w*)\}/g, (match, key) => {
-    if (key === '') {
-      key = unkeyed_index;
-      unkeyed_index++;
-    }
-    if (key == +key) {
-      return args[key] !== undefined ? args[key] : match;
-    }
-  });
-}
-
 class TranslationString {
   constructor(...args) {
     this.args = args;
@@ -47,10 +28,6 @@ class TranslationString {
   }
 
   #stitch() {
-    if (typeof this.args[0] === 'string') {
-      return stringReplace(this.args[0], this.args.slice(1));
-    }
-
     if (!(this.args[0] instanceof Array)) {
       throw new ValueError(
         `invalid args passed to TranslationString ${
