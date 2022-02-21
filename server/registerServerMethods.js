78667ad926a140cf18cf3a6b02436810853d0d6a
18alantom <2.alan.tom@gmail.com>
2022-02-21 12:07:34 +0530

    feat: use selected chart of accounts

--- server/registerServerMethods.js
+++ server/registerServerMethods.js
@@ -1,14 +1,5 @@
-import frappe from 'frappe';
-import importCharts from '../accounting/importCOA';
 import registerReportMethods from '../reports';
 
 export default function registerServerMethods() {
   registerReportMethods();
-
-  frappe.registerMethod({
-    method: 'import-coa',
-    async handler() {
-      await importCharts();
-    },
-  });
 }
