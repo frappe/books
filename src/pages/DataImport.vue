be68ab84f836dffeb505085124026451e4cf632a
18alantom <2.alan.tom@gmail.com>
2022-02-22 14:13:56 +0530

    feat: add assign imported labels bar

--- src/pages/DataImport.vue
+++ src/pages/DataImport.vue
@@ -7,61 +7,71 @@
         </h1>
       </template>
       <template #actions>
+        <DropdownWithActions
+          class="ml-2"
+          :actions="actions"
+          v-if="canCancel || importType"
+        />
         <Button
-          v-if="canCancel"
-          type="secondary"
-          class="text-white text-xs ml-2"
-          @click="cancel"
+          v-if="importType"
+          type="primary"
+          class="text-sm ml-2"
+          @click="handlePrimaryClick"
+          >{{ primaryLabel }}</Button
         >
-          {{ t`Cancel` }}
-        </Button>
       </template>
     </PageHeader>
-    <div class="flex justify-center flex-1 mb-8 mt-2">
-      <div
-        class="
-          border
-          rounded-lg
-          shadow
-          h-full
-          flex flex-col
-          justify-between
-          p-6
-        "
-        style="width: 600px"
-      >
-        <div>
-          <div class="flex flex-row justify-between items-center">
-            <FormControl
-              :df="importableDf"
-              input-class="bg-gray-100 text-gray-900 text-base"
-              class="w-1/4"
-              :value="importType"
-              @change="setImportType"
-            />
-            <p
-              class="text-base text-base"
-              :class="fileName ? 'text-gray-900' : 'text-gray-700'"
-            >
-              {{ helperText }}
-            </p>
-          </div>
-          <hr class="mt-6" />
+    <!-- <div class="flex justify-center flex-1 mb-8 mt-2"> -->
+    <div class="flex px-8 mt-2 text-base w-full flex-col gap-8">
+      <!-- Type selector -->
+      <div class="flex flex-row justify-start items-center w-full">
+        <FormControl
+          :df="importableDf"
+          input-class="bg-gray-100 text-gray-900 text-base"
+          class="w-40"
+          :value="importType"
+          size="small"
+          @change="setImportType"
+        />
+        <p
+          class="text-base text-base ml-2"
+          :class="fileName ? 'text-gray-900 font-semibold' : 'text-gray-700'"
+        >
+          <span v-if="fileName" class="font-normal"
+            >{{ t`Selected file` }}
+          </span>
+          {{ helperText }}{{ fileName ? ',' : '' }}
+          <span v-if="fileName" class="font-normal">
+            {{ t`verify imported data and click on` }} </span
+          >{{ ' ' }}<span v-if="fileName">{{ t`Import Data` }}</span
+          >.
+        </p>
+      </div>
+
+      <!-- Label Assigner -->
+      <div v-if="fileName">
+        <h2 class="text-lg font-semibold">{{ t`Assign Imported Labels` }}</h2>
+        <div class="gap-2 mt-4 grid grid-flow-col overflow-scroll pb-4">
+          <FormControl
+            :show-label="true"
+            size="small"
+            class="w-28"
+            input-class="bg-gray-100"
+            v-for="(f, k) in importer.assignableLabels"
+            :df="getAssignerField(f)"
+            :value="importer.assignedMap[f] ?? ''"
+            @change="(v) => onAssignedChange(f, v)"
+            :key="f + '-' + k"
+          />
         </div>
-        <div v-if="importType">
-          <hr class="mb-6" />
-          <div class="flex flex-row justify-between text-base">
-            <Button
-              class="w-1/4"
-              :padding="false"
-              @click="handleSecondaryClick"
-              >{{ secondaryLabel }}</Button
-            >
-            <Button class="w-1/4" type="primary" @click="handlePrimaryClick">{{
-              primaryLabel
-            }}</Button>
-          </div>
-        </div>
+      </div>
+
+      <!-- Data Verifier -->
+      <div v-if="fileName">
+        <h2 class="-mt-4 text-lg font-semibold">
+          {{ t`Verify Imported Data` }}
+        </h2>
+        <div></div>
       </div>
     </div>
   </div>
@@ -75,8 +85,9 @@ import Button from '@/components/Button.vue';
 import { ipcRenderer } from 'electron';
 import { IPC_ACTIONS } from '@/messages';
 import { getSavePath, saveData, showToast } from '@/utils';
+import DropdownWithActions from '@/components/DropdownWithActions.vue';
 export default {
-  components: { PageHeader, FormControl, Button },
+  components: { PageHeader, FormControl, Button, DropdownWithActions },
   data() {
     return {
       file: null,
@@ -85,6 +96,25 @@ export default {
     };
   },
   computed: {
+    actions() {
+      const cancelAction = {
+        component: {
+          template: '<span class="text-red-700" >{{ t`Cancel` }}</span>',
+        },
+        condition: () => true,
+        action: this.cancel,
+      };
+
+      const secondaryAction = {
+        component: {
+          template: `<span>${this.secondaryLabel}</span>`,
+        },
+        condition: () => true,
+        action: this.handleSecondaryClick,
+      };
+      return [secondaryAction, cancelAction];
+    },
+
     fileName() {
       if (!this.file) {
         return '';
@@ -161,6 +191,24 @@ export default {
       }
       await saveData(template, filePath);
     },
+    getAssignerField(targetLabel) {
+      const assigned = this.importer.assignedMap[targetLabel];
+      return {
+        fieldname: 'assignerField',
+        label: targetLabel,
+        placeholder: `Select Label`,
+        fieldtype: 'Select',
+        options: [
+          '',
+          ...(assigned ? [assigned] : []),
+          ...this.importer.unassignedLabels,
+        ],
+        default: assigned ?? '',
+      };
+    },
+    onAssignedChange(target, value) {
+      this.importer.assignedMap[target] = value;
+    },
     importData() {},
     toggleView() {},
     setImportType(importType) {
@@ -177,7 +225,7 @@ export default {
       const { success, canceled, filePath, data, name } =
         await ipcRenderer.invoke(IPC_ACTIONS.GET_FILE, options);
 
-      if (!success) {
+      if (!success && !canceled) {
         showToast({ message: this.t`File selection failed.`, type: 'error' });
       }
 
@@ -185,11 +233,23 @@ export default {
         return;
       }
 
+      const text = new TextDecoder().decode(data);
+      const isValid = this.importer.selectFile(text);
+      if (!isValid) {
+        showToast({
+          message: this.t`Bad import data. Could not select file.`,
+          type: 'error',
+        });
+        return;
+      }
+
       this.file = {
         name,
         filePath,
-        data: new TextDecoder().decode(data),
+        text,
       };
+
+      window.i = this.importer;
     },
   },
 };
