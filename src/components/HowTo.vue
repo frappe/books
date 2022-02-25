f87da9a6ef08faee38248c3110dee9ea33c22df8
18alantom <2.alan.tom@gmail.com>
2022-02-25 12:07:15 +0530

    feat: add a 'how to' component - to be used in place of the empty state

---
+++ src/components/HowTo.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    @click="openHelpLink"
+    class="
+      text-gray-900
+      shadow-md
+      px-3
+      py-2
+      flex
+      items-center
+      mb-3
+      z-10
+      bg-white
+      rounded-lg
+      text-base
+    "
+  >
+    <FeatherIcon class="h-6 w-6 mr-3 text-blue-400" name="help-circle" />
+    <p class="mr-1"><slot></slot></p>
+  </button>
+</template>
+<script>
+import { IPC_MESSAGES } from '@/messages';
+import { ipcRenderer } from 'electron';
+import FeatherIcon from './FeatherIcon.vue';
+
+export default {
+  props: {
+    link: String,
+  },
+  methods: {
+    openHelpLink() {
+      ipcRenderer.send(IPC_MESSAGES.OPEN_EXTERNAL, this.link);
+    },
+  },
+  components: { FeatherIcon },
+};
+</script>
