81eda2ea1319f257252631af085cd396f16968b2
18alantom <2.alan.tom@gmail.com>
2022-03-08 12:16:18 +0530

    feat: update number series with references

---
+++ patches/0.3.2/moveNumberSeriesFromSettings.js
@@ -0,0 +1,16 @@
+import { invertMap } from '@/utils';
+import frappe from 'frappe';
+import { DEFAULT_NUMBER_SERIES } from 'frappe/utils/consts';
+
+async function setReferencesOnNumberSeries() {
+  const map = invertMap(DEFAULT_NUMBER_SERIES);
+  const rows = await frappe.db.knex('NumberSeries');
+  for (const row of rows) {
+    row.referenceType = map[row.name];
+  }
+  await frappe.db.prestigeTheTable('NumberSeries', rows);
+}
+
+export default async function execute() {
+  await setReferencesOnNumberSeries();
+}
