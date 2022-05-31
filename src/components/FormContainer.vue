97d2885475fa254210205725a177d7b7bd834455
18alantom <2.alan.tom@gmail.com>
2022-05-31 15:19:28 +0530

    incr: use form container for form-ish layouts

---
+++ src/components/FormContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Page Header (Title, Buttons, etc) -->
+    <PageHeader :backLink="backLink" :title="title">
+      <slot name="header" />
+    </PageHeader>
+
+    <!-- Invoice Form -->
+    <div
+      class="
+        border
+        rounded-lg
+        shadow
+        flex flex-col
+        self-center
+        w-form
+        h-full
+        my-4
+        bg-white
+      "
+    >
+      <slot name="body" />
+    </div>
+  </div>
+</template>
+<script>
+import PageHeader from './PageHeader.vue';
+export default {
+  components: { PageHeader },
+  props: {
+    backLink: { type: Boolean, default: false },
+    title: { type: String, default: '' },
+  },
+};
+</script>
