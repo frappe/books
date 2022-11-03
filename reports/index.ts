e1123ecc3a5d0504f03110cc2425b4f3d231b23c
18alantom <2.alan.tom@gmail.com>
2022-11-03 20:25:08 +0530

    incr: add basic stock ledger (w.i.p)

--- reports/index.ts
+++ reports/index.ts
@@ -2,6 +2,7 @@ import { BalanceSheet } from './BalanceSheet/BalanceSheet';
 import { GeneralLedger } from './GeneralLedger/GeneralLedger';
 import { GSTR1 } from './GoodsAndServiceTax/GSTR1';
 import { GSTR2 } from './GoodsAndServiceTax/GSTR2';
+import { StockLedger } from './inventory/StockLedger';
 import { ProfitAndLoss } from './ProfitAndLoss/ProfitAndLoss';
 import { Report } from './Report';
 import { TrialBalance } from './TrialBalance/TrialBalance';
@@ -13,4 +14,5 @@ export const reports = {
   TrialBalance,
   GSTR1,
   GSTR2,
+  StockLedger,
 } as Record<string, typeof Report>;
