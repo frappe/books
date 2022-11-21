fff0b6a33e7e550a959672465507b0e2fac0dbd3
18alantom <2.alan.tom@gmail.com>
2022-11-21 12:45:57 +0530

    incr: add default location

--- backend/patches/addUOMs.ts
+++ backend/patches/addUOMs.ts
@@ -1,7 +1,33 @@
 import { ModelNameEnum } from '../../models/types';
-import { defaultUOMs } from '../../utils/defaults';
 import { DatabaseManager } from '../database/manager';
 import { getDefaultMetaFieldValueMap } from '../helpers';
+
+const defaultUOMs = [
+  {
+    name: `Unit`,
+    isWhole: true,
+  },
+  {
+    name: `Kg`,
+    isWhole: false,
+  },
+  {
+    name: `Gram`,
+    isWhole: false,
+  },
+  {
+    name: `Meter`,
+    isWhole: false,
+  },
+  {
+    name: `Hour`,
+    isWhole: false,
+  },
+  {
+    name: `Day`,
+    isWhole: false,
+  },
+];
 
 async function execute(dm: DatabaseManager) {
   for (const uom of defaultUOMs) {
