fe68edd94d728ca4b22f4ff05330b118e5528921
18alantom <2.alan.tom@gmail.com>
2023-01-16 09:43:59 +0530

    chore: add translation credits - convert a few funcs to predicate funcs

--- fyo/model/doc.ts
+++ fyo/model/doc.ts
@@ -543,7 +543,7 @@ export class Doc extends Observable<DocValue | Doc[]> {
       }
 
       if (isPesa(value)) {
-        value = (value as Money).copy();
+        value = value.copy();
       }
 
       if (value === null && this.schema.isSingle) {
@@ -966,7 +966,8 @@ export class Doc extends Observable<DocValue | Doc[]> {
             throw err;
           }
         }
-        return value as Money;
+
+        return value;
       })
       .reduce((a, b) => a.add(b), this.fyo.pesa(0));
 
