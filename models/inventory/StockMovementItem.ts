08152830acdea2e1c6193adfedcd3a1ec848d39c
akshayitzme <akshaypooloth1@gmail.com>
2023-02-15 14:39:56 +0530

    feat: uom conversion factor

--- models/inventory/StockMovementItem.ts
+++ models/inventory/StockMovementItem.ts
@@ -1,3 +1,5 @@
+import { t } from 'fyo';
+import { DocValue } from 'fyo/core/types';
 import { Doc } from 'fyo/model/doc';
 import {
   FiltersMap,
@@ -9,6 +11,7 @@ import {
 import { ValidationError } from 'fyo/utils/errors';
 import { ModelNameEnum } from 'models/types';
 import { Money } from 'pesa';
+import { safeParseFloat } from 'utils/index';
 import { StockMovement } from './StockMovement';
 import { MovementType } from './types';
 
@@ -18,6 +21,10 @@ export class StockMovementItem extends Doc {
   fromLocation?: string;
   toLocation?: string;
   quantity?: number;
+  transferQty?: number;
+  stockUOM?: string;
+  uom?: string;
+  UOMConversionFactor?: number;
   rate?: Money;
   amount?: Money;
   parentdoc?: StockMovement;
@@ -89,6 +96,49 @@ export class StockMovementItem extends Doc {
       },
       dependsOn: ['movementType'],
     },
+    stockUOM: {
+      formula: async () => {
+        const { unit } = await this.fyo.db.get(
+          ModelNameEnum.Item,
+          this.item!,
+          'unit'
+        );
+        return unit;
+      },
+      dependsOn: ['item'],
+    },
+    UOMConversionFactor: {
+      formula: async () => {
+        const conversionFactor = await this.fyo.db.getAll(
+          ModelNameEnum.UOMConversionFactor,
+          {
+            fields: ['value'],
+            filters: { parent: this.item! },
+          }
+        );
+        return safeParseFloat(conversionFactor[0].value);
+      },
+      dependsOn: ['uom'],
+    },
+    quantity: {
+      formula: async () => {
+        if (!this.item) {
+          return this.quantity as number;
+        }
+
+        const itemDoc = await this.fyo.doc.getDoc(
+          ModelNameEnum.Item,
+          this.item as string
+        );
+        const unitDoc = itemDoc.getLink('uom');
+        if (unitDoc?.isWhole) {
+          return Math.round(this.transferQty! * this.UOMConversionFactor!);
+        }
+
+        return safeParseFloat(this.transferQty! * this.UOMConversionFactor!);
+      },
+      dependsOn: ['quantity', 'transferQty', 'UOMConversionFactor'],
+    },
   };
 
   validations: ValidationMap = {
@@ -113,6 +163,16 @@ export class StockMovementItem extends Doc {
           this.fyo.t`Only From or To can be set for Manufacture`
         );
       }
+    },
+    uom: async (value: DocValue) => {
+      const item = await this.fyo.db.getAll(ModelNameEnum.UOMConversionFactor, {
+        fields: ['parent'],
+        filters: { uom: value as string, parent: this.item! },
+      });
+      if (item.length < 1)
+        throw new ValidationError(
+          t`UOM ${value as string} is not applicable for item ${this.item!}`
+        );
     },
   };
 
