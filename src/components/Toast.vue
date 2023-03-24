4fa3169ce42537377d88b8a2356b52dd84e463a7
18alantom <2.alan.tom@gmail.com>
2023-03-24 13:42:03 +0530

    refactor: simplify Toast with Teleport

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,47 +1,60 @@
 <template>
-  <div
-    class="
-      text-gray-900
-      shadow-lg
-      px-3
-      py-2
-      flex
-      items-center
-      mb-3
-      w-96
-      z-30
-      bg-white
-      rounded-lg
-    "
-    style="transition: opacity 150ms ease-in"
-    :style="{ opacity }"
-    v-if="show"
-  >
-    <feather-icon :name="iconName" class="w-6 h-6 me-3" :class="iconColor" />
-    <div @click="actionClicked" :class="actionText ? 'cursor-pointer' : ''">
-      <p class="text-base">{{ message }}</p>
-      <button
-        v-if="actionText"
-        class="text-sm text-gray-700 hover:text-gray-800"
+  <Teleport to="#toast-container">
+    <Transition>
+      <div
+        v-if="open"
+        class="
+          inner
+          text-gray-900
+          shadow-lg
+          px-3
+          py-2
+          flex
+          items-center
+          mb-3
+          w-toast
+          z-30
+          bg-white
+          rounded-lg
+        "
+        style="pointer-events: auto"
       >
-        {{ actionText }}
-      </button>
-    </div>
-    <feather-icon
-      name="x"
-      class="w-4 h-4 ms-auto text-gray-600 cursor-pointer hover:text-gray-800"
-      @click="closeToast"
-    />
-  </div>
+        <feather-icon
+          :name="iconName"
+          class="w-6 h-6 me-3"
+          :class="iconColor"
+        />
+        <div @click="actionClicked" :class="actionText ? 'cursor-pointer' : ''">
+          <p class="text-base">{{ message }}</p>
+          <button
+            v-if="actionText"
+            class="text-sm text-gray-700 hover:text-gray-800"
+          >
+            {{ actionText }}
+          </button>
+        </div>
+        <feather-icon
+          name="x"
+          class="
+            w-4
+            h-4
+            ms-auto
+            text-gray-600
+            cursor-pointer
+            hover:text-gray-800
+          "
+          @click="closeToast"
+        />
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 <script lang="ts">
 import { getColorClass } from 'src/utils/colors';
 import { ToastDuration, ToastType } from 'src/utils/types';
 import { toastDurationMap } from 'src/utils/ui';
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, nextTick, PropType } from 'vue';
 import FeatherIcon from './FeatherIcon.vue';
-
-type TimeoutId = ReturnType<typeof setTimeout>;
 
 export default defineComponent({
   components: {
@@ -49,15 +62,7 @@ export default defineComponent({
   },
   data() {
     return {
-      opacity: 0,
-      show: true,
-      opacityTimeoutId: null,
-      cleanupTimeoutId: null,
-    } as {
-      opacity: number;
-      show: boolean;
-      opacityTimeoutId: null | TimeoutId;
-      cleanupTimeoutId: null | TimeoutId;
+      open: false,
     };
   },
   props: {
@@ -90,20 +95,10 @@ export default defineComponent({
       return getColorClass(this.color ?? 'gray', 'text', 400);
     },
   },
-  mounted() {
+  async mounted() {
     const duration = toastDurationMap[this.duration];
-    setTimeout(() => {
-      this.opacity = 1;
-    }, 50);
-
-    this.opacityTimeoutId = setTimeout(() => {
-      this.opacity = 0;
-    }, duration);
-
-    this.cleanupTimeoutId = setTimeout(() => {
-      this.show = false;
-      this.cleanup();
-    }, duration + 300);
+    await nextTick(() => (this.open = true));
+    setTimeout(this.closeToast, duration);
   },
   methods: {
     actionClicked() {
@@ -111,30 +106,24 @@ export default defineComponent({
       this.closeToast();
     },
     closeToast() {
-      if (this.opacityTimeoutId != null) {
-        clearTimeout(this.opacityTimeoutId);
-      }
-      if (this.cleanupTimeoutId != null) {
-        clearTimeout(this.cleanupTimeoutId);
-      }
-
-      this.opacity = 0;
-      setTimeout(() => {
-        this.show = false;
-        this.cleanup();
-      }, 300);
-    },
-    cleanup() {
-      const element = this.$el;
-      if (!(element instanceof Element)) {
-        return;
-      }
-
-      Array.from(element.parentElement?.children ?? [])
-        .filter((el) => !el.innerHTML)
-        .splice(1)
-        .forEach((el) => el.remove());
+      this.open = false;
     },
   },
 });
 </script>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 150ms ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter-to,
+.v-leave-from {
+  opacity: 1;
+}
+</style>
