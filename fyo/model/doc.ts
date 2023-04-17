5a3b87333e808e4f441e5029b98d34dae9248441
18alantom <2.alan.tom@gmail.com>
2023-04-17 09:59:12 +0530

    fix: uninserted doc cache router break - explicit drop from cache on delete if notinserted

--- fyo/model/doc.ts
+++ fyo/model/doc.ts
@@ -915,6 +915,10 @@ export class Doc extends Observable<DocValue | Doc[]> {
   }
 
   async delete() {
+    if (this.notInserted && this.name) {
+      this.fyo.doc.removeFromCache(this.schemaName, this.name);
+    }
+
     if (!this.canDelete) {
       return;
     }
