0bfe923a516061014bbfd43735f764af6083abb6
18alantom <2.alan.tom@gmail.com>
2023-05-09 09:55:11 +0530

    fix(ui/ux): move back button to the left - add forward button

--- src/utils/refs.ts
+++ src/utils/refs.ts
@@ -1,5 +1,9 @@
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 export const showSidebar = ref(true);
 export const docsPathRef = ref<string>('');
 export const systemLanguageRef = ref<string>('');
+export const historyState = reactive({
+  forward: !!history.state.forward,
+  back: !!history.state.back,
+});
