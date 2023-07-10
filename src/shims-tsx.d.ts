42b10bb7ab2e89890ac4339a4788d3dadea21aa6
18alantom <2.alan.tom@gmail.com>
2023-07-10 13:04:14 +0530

    incr: add preload scripts

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,5 +1,7 @@
+import type { IPC } from 'main/preload';
 import Vue, { VNode } from 'vue';
 
+declare const ipc: IPC;
 declare global {
   namespace JSX {
     type Element = VNode;
@@ -8,5 +10,9 @@ declare global {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       [elem: string]: any;
     }
+  }
+
+  interface Window {
+    ipc: IPC;
   }
 }
