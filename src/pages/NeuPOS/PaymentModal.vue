dc371077b78e8ac59c21d7e2285632e71142b777
akshayitzme <akshaypooloth1@gmail.com>
2023-08-23 11:31:19 +0530

    feat: payment modal init

---
+++ src/pages/NeuPOS/PaymentModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <Modal class="w-2/5 ml-auto mr-3.5" :open-modal="openModal">
+    <div class="px-4 py-6 grid" style="height: 95vh">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, cumque.
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Modal from 'src/components/Modal.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PaymentModal',
+  components: { Modal },
+});
+</script>
