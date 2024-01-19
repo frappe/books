dcbfa9f1c5deb235c764f141220af562bf919b17
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2024-01-19 23:23:03 +0100

    fix: setPaymentReferenceType patch for payments for versions >0.18 <0.20

--- backend/patches/setPaymentReferenceType.ts
+++ backend/patches/setPaymentReferenceType.ts
@@ -2,7 +2,10 @@ import { DatabaseManager } from '../database/manager';
 
 async function execute(dm: DatabaseManager) {
   await dm.db!.knex!('Payment')
-    .where({ referenceType: null })
+    .where({ referenceType: null, paymentType: 'Pay' })
+    .update({ referenceType: 'PurchaseInvoice' });
+  await dm.db!.knex!('Payment')
+    .where({ referenceType: null, paymentType: 'Receive' })
     .update({ referenceType: 'SalesInvoice' });
 }
 
