233249a970a6866f554f110daa286eafa0d901cf
pu-raihan <raihanbinmary@gmail.com>
2024-09-07 07:25:38 +0530

    fix: added dark scrollbar for the whole html document

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,6 +1,10 @@
 export function setDarkMode(darkMode: boolean): void {
   if (darkMode) {
-    document.documentElement.classList.add('dark');
+    document.documentElement.classList.add(
+      'dark',
+      'custom-scroll',
+      'custom-scroll-thumb1'
+    );
     return;
   }
   document.documentElement.classList.remove('dark');
