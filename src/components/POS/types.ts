9a95d6fc96177d30e125ab1c120fcd8ee901eb65
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-10-08 12:24:05 +0530

    feat: add modal type

--- src/components/POS/types.ts
+++ src/components/POS/types.ts
@@ -1,21 +1,28 @@
-import { Money } from "pesa";
+import { Money } from 'pesa';
 
 export type ItemQtyMap = {
-    [item: string]: { availableQty: number;[batch: string]: number };
-}
+  [item: string]: { availableQty: number; [batch: string]: number };
+};
 
 export type ItemSerialNumbers = { [item: string]: string };
 
-export type DiscountType = "percent" | "amount";
+export type DiscountType = 'percent' | 'amount';
 
-export type ModalName = 'ShiftOpen' | 'ShiftClose' | 'Payment' | 'LoyaltyProgram' | 'SavedInvoice' | 'RouteToInvoiceList'
+export type ModalName =
+  | 'ShiftOpen'
+  | 'ShiftClose'
+  | 'Payment'
+  | 'LoyaltyProgram'
+  | 'SavedInvoice'
+  | 'RouteToInvoiceList'
+  | 'CouponCode';
 
 export interface POSItem {
-    image?:string,
-    name: string,
-    rate: Money,
-    availableQty: number,
-    unit: string,
-    hasBatch: boolean,
-    hasSerialNumber: boolean,
+  image?: string;
+  name: string;
+  rate: Money;
+  availableQty: number;
+  unit: string;
+  hasBatch: boolean;
+  hasSerialNumber: boolean;
 }
