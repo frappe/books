1a7ebc918e347d8564890239a5b93953e494d00c
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-10-14 16:05:06 +0530

    fix: move pricing rule to sales instead of common

--- src/utils/sidebarConfig.ts
+++ src/utils/sidebarConfig.ts
@@ -217,6 +217,13 @@ function getCompleteSidebar(): SidebarConfig {
           hidden: () => !fyo.singles.AccountingSettings?.enableLead,
         },
         {
+          label: t`Pricing Rule`,
+          name: 'pricing-rule',
+          route: '/list/PricingRule',
+          schemaName: 'PricingRule',
+          hidden: () => !fyo.singles.AccountingSettings?.enablePricingRule,
+        },
+        {
           label: t`Coupon Code`,
           name: 'coupon-code',
           route: `/list/CouponCode`,
@@ -292,13 +299,6 @@ function getCompleteSidebar(): SidebarConfig {
           route: '/list/PriceList',
           schemaName: 'PriceList',
           hidden: () => !fyo.singles.AccountingSettings?.enablePriceList,
-        },
-        {
-          label: t`Pricing Rule`,
-          name: 'pricing-rule',
-          route: '/list/PricingRule',
-          schemaName: 'PricingRule',
-          hidden: () => !fyo.singles.AccountingSettings?.enablePricingRule,
         },
       ] as SidebarItem[],
     },
