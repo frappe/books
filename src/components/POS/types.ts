ad5d4075dafed3cc2e937926746389899d4d6eaf
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-10-14 10:48:34 +0530

    feat: add SavedInvoice type

--- src/components/POS/types.ts
+++ src/components/POS/types.ts
@@ -8,7 +8,7 @@ export type ItemSerialNumbers = { [item: string]: string };
 
 export type DiscountType = "percent" | "amount";
 
-export type ModalName = 'ShiftOpen' | 'ShiftClose' | 'Payment' | 'LoyaltyProgram' | 'RouteToInvoiceList'
+export type ModalName = 'ShiftOpen' | 'ShiftClose' | 'Payment' | 'LoyaltyProgram' | 'SavedInvoice' | 'RouteToInvoiceList'
 
 export interface POSItem {
     image?:string,
