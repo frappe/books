77ec230e1817e1dcca046bfa379f0c7fb9dac775
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-11-08 14:55:36 +0530

    fix: update 'select' field type

--- src/components/Controls/Select.vue
+++ src/components/Controls/Select.vue
@@ -4,65 +4,89 @@
       {{ df.label }}
     </div>
     <div
+      v-on-outside-click="() => (dropdownVisible = false)"
       class="flex items-center justify-between"
-      :class="[inputClasses, containerClasses]"
+      :class="[
+        inputClasses,
+        containerClasses,
+        dropdownVisible ? 'dark:hover:bg-gray-850' : '',
+      ]"
     >
-      <select
-        class="
-          appearance-none
-          bg-transparent
-          focus:outline-none
-          w-11/12
-          cursor-pointer
-          custom-scroll custom-scroll-thumb2
-        "
-        :class="{
-          'pointer-events-none': isReadOnly,
-          'text-gray-500': !value,
-        }"
-        :value="value"
-        @change="onChange"
-        @focus="(e) => $emit('focus', e)"
-      >
-        <option
-          v-if="inputPlaceholder && !showLabel"
-          value=""
-          disabled
-          selected
-          class="text-black dark:text-gray-200 dark:bg-gray-850"
-        >
-          {{ inputPlaceholder }}
-        </option>
-        <option
-          v-for="option in options"
-          :key="option.value"
-          :value="option.value"
-          class="text-black dark:text-gray-200 dark:bg-gray-850"
-        >
-          {{ option.label }}
-        </option>
-      </select>
-      <svg
-        v-if="!isReadOnly"
-        class="w-3 h-3"
-        style="background: inherit; margin-right: -3px"
-        viewBox="0 0 5 10"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M1 2.636L2.636 1l1.637 1.636M1 7.364L2.636 9l1.637-1.636"
-          class="stroke-current"
-          :class="
-            showMandatory
-              ? 'text-red-400 dark:text-red-600'
-              : 'text-gray-400 dark:text-gray-600'
+      <div class="relative w-full" @click="toggleDropdown">
+        <div
+          class="
+            flex
+            items-center
+            justify-between
+            bg-transparent
+            w-full
+            cursor-pointer
+            custom-scroll custom-scroll-thumb2
           "
-          fill="none"
-          fill-rule="evenodd"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
+          :class="{
+            'pointer-events-none': isReadOnly,
+            'text-gray-500': !value,
+          }"
+        >
+          <span v-if="value" class="cursor-text w-full">{{ value }}</span>
+          <span v-else>{{ inputPlaceholder }}</span>
+          <svg
+            v-if="!isReadOnly"
+            class="w-3 h-3"
+            style="background: inherit; margin-right: -3px"
+            viewBox="0 0 5 10"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 2.636L2.636 1l1.637 1.636M1 7.364L2.636 9l1.637-1.636"
+              class="stroke-current"
+              :class="
+                showMandatory
+                  ? 'text-red-400 dark:text-red-600'
+                  : 'text-gray-400 dark:text-gray-600'
+              "
+              fill="none"
+              fill-rule="evenodd"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+        <div
+          v-if="dropdownVisible"
+          class="
+            absolute
+            z-10
+            mt-4
+            w-full
+            bg-white
+            dark:bg-gray-850
+            border border-gray-300
+            dark:border-gray-700
+            cursor-pointer
+            rounded-md
+            shadow-lg
+          "
+        >
+          <ul
+            class="
+              max-h-40
+              p-1
+              overflow-auto
+              custom-scroll custom-scroll-thumb1
+            "
+          >
+            <li
+              v-for="option in options"
+              :key="option.value"
+              @click="selectOption(option)"
+              class="p-1.5 rounded-md hover:bg-gray-100 dark:hover:bg-gray-875"
+            >
+              {{ option.label }}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -76,6 +100,11 @@ export default defineComponent({
   name: 'Select',
   extends: Base,
   emits: ['focus'],
+  data() {
+    return {
+      dropdownVisible: false,
+    };
+  },
   computed: {
     options(): SelectOption[] {
       if (this.df.fieldtype !== 'Select') {
@@ -86,16 +115,15 @@ export default defineComponent({
     },
   },
   methods: {
-    onChange(e: Event) {
-      const target = e.target;
-      if (
-        !(target instanceof HTMLSelectElement) &&
-        !(target instanceof HTMLInputElement)
-      ) {
-        return;
+    toggleDropdown() {
+      if (!this.isReadOnly) {
+        this.dropdownVisible = !this.dropdownVisible;
       }
-
-      this.triggerChange(target.value);
+    },
+    selectOption(option: SelectOption) {
+      this.dropdownVisible = false;
+      this.triggerChange(option.value);
+      this.dropdownVisible = !this.dropdownVisible;
     },
   },
 });
