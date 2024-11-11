57dd1062a3ffbc2a920222fc1556cac38fd9b8da
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-11-11 11:02:19 +0530

    fix: add checkmark for selected status

--- src/components/Controls/Select.vue
+++ src/components/Controls/Select.vue
@@ -80,8 +80,30 @@
               v-for="option in options"
               :key="option.value"
               @click="selectOption(option)"
-              class="p-1.5 rounded-md hover:bg-gray-100 dark:hover:bg-gray-875"
+              class="
+                p-1.5
+                rounded-md
+                hover:bg-gray-100
+                dark:hover:bg-gray-875
+                flex
+              "
+              :class="value !== option.label ? 'pl-6' : 'pl-2'"
             >
+              <svg
+                v-if="value === option.label"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="13"
+                height="20"
+                viewBox="0 0 50 50"
+                fill="currentColor"
+                class="mr-1"
+              >
+                <path
+                  d="M 41.9375 8.625 C 41.273438 8.648438 40.664063 9 40.3125 9.5625 L 21.5 38.34375 L 9.3125 27.8125 C 8.789063 27.269531 8.003906 27.066406 7.28125 27.292969 C 6.5625 27.515625 6.027344 28.125 5.902344 28.867188 C 5.777344 29.613281 6.078125 30.363281 6.6875 30.8125 L 20.625 42.875 C 21.0625 43.246094 21.640625 43.410156 22.207031 43.328125 C 22.777344 43.242188 23.28125 42.917969 23.59375 42.4375 L 43.6875 11.75 C 44.117188 11.121094 44.152344 10.308594 43.78125 9.644531 C 43.410156 8.984375 42.695313 8.589844 41.9375 8.625 Z"
+                ></path>
+              </svg>
               {{ option.label }}
             </li>
           </ul>
