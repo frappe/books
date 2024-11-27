e1f334113e0c438d7ab0c91974f29be31120da8e
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-11-27 17:52:00 +0530

    fix: add Pos Emits type

--- src/components/POS/types.ts
+++ src/components/POS/types.ts
@@ -24,6 +24,7 @@ export type PosEmits =
   | 'setCashAmount'
   | 'setCouponsCount'
   | 'routeToSinvList'
+  | 'applyPricingRule'
   | 'setTransferRefNo'
   | 'setLoyaltyPoints'
   | 'setTransferAmount'
