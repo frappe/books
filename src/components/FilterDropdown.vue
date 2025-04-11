30bc8c56753a179949885fbd64ec87de0e7c29f2
suhail <suhailanzar00@gmail.com>
2025-04-11 14:27:30 +0530

    fix: improve docs filtering and add clear option

--- src/components/FilterDropdown.vue
+++ src/components/FilterDropdown.vue
@@ -1,9 +1,12 @@
 <template>
   <Popover
+    ref="filterPopover"
     v-if="fields.length"
     placement="bottom-end"
     @close="emitFilterChange"
+    :close-on-click-outside="true"
   >
+    :close-on-click-content="false" >
     <template #target="{ togglePopover }">
       <Button :icon="true" @click="togglePopover()">
         <span class="flex items-center">
@@ -24,96 +27,104 @@
       </Button>
     </template>
     <template #content>
-      <div>
-        <div class="p-2">
-          <template v-if="explicitFilters.length">
-            <div class="flex flex-col gap-2">
+      <div class="p-2">
+        <template v-if="explicitFilters.length">
+          <div class="flex flex-col gap-2">
+            <div
+              v-for="(filter, i) in explicitFilters"
+              :key="filter.fieldname + getRandomString()"
+              class="flex items-center justify-between text-base gap-2"
+            >
               <div
-                v-for="(filter, i) in explicitFilters"
-                :key="filter.fieldname + getRandomString()"
-                class="flex items-center justify-between text-base gap-2"
+                class="
+                  cursor-pointer
+                  w-4
+                  h-4
+                  flex
+                  items-center
+                  justify-center
+                  text-gray-600
+                  dark:text-gray-400
+                  hover:text-gray-800
+                  dark:hover:text-gray-300
+                  rounded-md
+                  group
+                "
               >
-                <div
-                  class="
-                    cursor-pointer
-                    w-4
-                    h-4
-                    flex
-                    items-center
-                    justify-center
-                    text-gray-600
-                    dark:text-gray-400
-                    hover:text-gray-800
-                    dark:hover:text-gray-300
-                    rounded-md
-                    group
-                  "
-                >
-                  <span class="hidden group-hover:inline-block">
-                    <feather-icon
-                      name="x"
-                      class="w-4 h-4 cursor-pointer"
-                      :button="true"
-                      @click="removeFilter(filter)"
-                    />
-                  </span>
-                  <span class="group-hover:hidden">
-                    {{ i + 1 }}
-                  </span>
-                </div>
-                <Select
-                  :border="true"
-                  size="small"
-                  class="w-24"
-                  :df="{
-                    label: t`Field`,
-                    placeholder: t`Field`,
-                    fieldname: 'fieldname',
-                    fieldtype: 'Select',
-                    options: fieldOptions,
-                  }"
-                  :value="filter.fieldname"
-                  :close-drop-down="false"
-                  @change="(value) => updateNewFilters(i, 'fieldname', value)"
-                />
-                <Select
-                  :border="true"
-                  size="small"
-                  class="w-24"
-                  :df="{
-                    label: t`Condition`,
-                    placeholder: t`Condition`,
-                    fieldname: 'condition',
-                    fieldtype: 'Select',
-                    options: conditions,
-                  }"
-                  :value="filter.condition"
-                  :close-drop-down="false"
-                  @change="(value) => updateNewFilters(i, 'condition', value)"
-                />
-                <Data
-                  :border="true"
-                  size="small"
-                  class="w-24"
-                  :df="{
-                    label: t`Value`,
-                    placeholder: t`Value`,
-                    fieldname: 'value',
-                    fieldtype: 'Data',
-                  }"
-                  :value="String(filter.value)"
-                  :close-drop-down="false"
-                  @change="(value) => updateNewFilters(i, 'value', value)"
-                />
+                <span class="hidden group-hover:inline-block">
+                  <feather-icon
+                    name="x"
+                    class="w-4 h-4 cursor-pointer"
+                    :button="true"
+                    @click="removeFilter(i)"
+                  />
+                </span>
+                <span class="group-hover:hidden">
+                  {{ i + 1 }}
+                </span>
               </div>
+              <Select
+                :border="true"
+                size="small"
+                class="w-24"
+                :df="{
+                  label: t`Field`,
+                  placeholder: t`field`,
+                  fieldname: 'fieldname',
+                  fieldtype: 'Select',
+                  options: fieldOptions,
+                }"
+                :value="filter.fieldname"
+                :close-drop-down="false"
+                @mousedown.stop
+                @click.stop
+                @change="(value) => updateNewFilters(i, 'fieldname', value)"
+              />
+
+              <Select
+                :border="true"
+                size="small"
+                class="w-24"
+                :df="{
+                  label: t`Condition`,
+                  placeholder: t`Condition`,
+                  fieldname: 'condition',
+                  fieldtype: 'Select',
+                  options: conditions,
+                }"
+                :value="filter.condition"
+                :close-drop-down="false"
+                @mousedown.stop
+                @click.stop
+                @change="(value) => updateNewFilters(i, 'condition', value)"
+              />
+
+              <Data
+                :border="true"
+                size="small"
+                class="w-24"
+                :df="{
+                  label: t`Value`,
+                  placeholder: t`Value`,
+                  fieldname: 'value',
+                  fieldtype: 'Data',
+                }"
+                :value="String(filter.value)"
+                :close-drop-down="false"
+                @mousedown.stop
+                @click.stop
+                @change="(value) => updateNewFilters(i, 'value', value)"
+              />
             </div>
-          </template>
-          <template v-else>
-            <span class="text-base text-gray-600 dark:text-gray-500">{{
-              t`No filters selected`
-            }}</span>
-          </template>
-        </div>
+          </div>
+        </template>
+        <template v-else>
+          <span class="text-base text-gray-600 dark:text-gray-500">{{
+            t`No filters selected`
+          }}</span>
+        </template>
+      </div>
+      <div class="flex justify-between border-t dark:border-gray-800">
         <div
           class="
             text-base
@@ -128,10 +139,52 @@
             hover:bg-gray-100
             dark:hover:bg-gray-875
           "
-          @click="addNewFilter"
+          @click.stop="addNewFilter"
         >
           <feather-icon name="plus" class="w-4 h-4" />
           <span class="ms-2">{{ t`Add a filter` }}</span>
+        </div>
+
+        <div class="flex">
+          <div
+            v-if="filters.length"
+            class="
+              text-base
+              p-2
+              flex
+              items-center
+              text-gray-600
+              dark:text-gray-500
+              cursor-pointer
+              hover:bg-gray-100
+              dark:hover:bg-gray-875
+            "
+            @click="clearAllFilters"
+          >
+            <feather-icon name="trash-2" class="w-4 h-4" />
+            <span class="ms-2">{{ t`Clear` }}</span>
+          </div>
+
+          <div
+            v-if="filters.length"
+            @click="applyFilters"
+            class="
+              text-base
+              border-t
+              dark:border-gray-800
+              p-2
+              flex
+              items-center
+              text-gray-600
+              dark:text-gray-500
+              cursor-pointer
+              hover:bg-gray-100
+              dark:hover:bg-gray-875
+            "
+          >
+            <feather-icon name="search" class="w-4 h-4" />
+            <span class="ml-2 text-sm">{{ t`Apply` }}</span>
+          </div>
         </div>
       </div>
     </template>
@@ -151,14 +204,14 @@ import Popover from './Popover.vue';
 import { QueryFilter } from 'utils/db/types';
 
 const conditions = [
-  { label: t`Is`, value: '=' },
-  { label: t`Is Not`, value: '!=' },
-  { label: t`Contains`, value: 'like' },
-  { label: t`Does Not Contain`, value: 'not like' },
-  { label: t`Greater Than`, value: '>' },
-  { label: t`Less Than`, value: '<' },
-  { label: t`Is Empty`, value: 'is null' },
-  { label: t`Is Not Empty`, value: 'is not null' },
+  { label: '=', value: '=' },
+  { label: '!=', value: '!=' },
+  { label: 'like', value: 'like' },
+  { label: 'not like', value: 'not like' },
+  { label: '>', value: '>' },
+  { label: '<', value: '<' },
+  { label: 'is null', value: 'is null' },
+  { label: 'is not null', value: 'is not null' },
 ] as const;
 
 type Condition = typeof conditions[number]['value'];
@@ -168,6 +221,13 @@ type Filter = {
   condition: Condition;
   value: QueryFilter[string];
   implicit: boolean;
+};
+
+type PopoverRef = {
+  close?: () => void;
+  hide?: () => void;
+  togglePopover?: (state: boolean) => void;
+  $el?: HTMLElement;
 };
 
 export default defineComponent({
@@ -213,7 +273,7 @@ export default defineComponent({
     },
     fieldOptions(): { label: string; value: string }[] {
       return this.fields.map((df) => ({
-        label: df.label,
+        label: df.fieldname,
         value: df.fieldname,
       }));
     },
@@ -253,20 +313,40 @@ export default defineComponent({
       value: Filter['value'],
       implicit?: boolean
     ): void {
-      this.filters.push({ fieldname, condition, value, implicit: !!implicit });
-      this.newFilters.push({
-        fieldname,
-        condition,
-        value,
-        implicit: !!implicit,
-      });
+      const newFilter = { fieldname, condition, value, implicit: !!implicit };
+      this.filters.push(newFilter);
+      this.newFilters.push(newFilter);
     },
-    removeFilter(filter: Filter): void {
-      this.filters = this.filters.filter((f) => f !== filter);
+
+    applyFilters() {
+      this.emitFilterChange();
+      const popover = this.$refs.filterPopover as PopoverRef;
+      if (popover?.close) popover.close();
+      else if (popover?.hide) popover.hide();
+      else if (popover?.togglePopover) popover.togglePopover(false);
+      else if (popover?.$el) popover.$el.click();
     },
-    updateNewFilters(index: number, key: keyof Filter, value: Filter['value']) {
+
+    removeFilter(index: number): void {
+      this.filters.splice(index, 1);
+      this.newFilters.splice(index, 1);
+    },
+
+    clearAllFilters(): void {
+      this.filters = [];
+      this.newFilters = [];
+
+      this.$emit('change', {});
+    },
+
+    updateNewFilters<K extends keyof Filter>(
+      index: number,
+      key: K,
+      value: Filter[K]
+    ) {
       this.newFilters![index][key] = value;
     },
+
     setFilter(filters: QueryFilter, implicit?: boolean): void {
       this.filters = [];
       this.newFilters = [];
@@ -289,25 +369,34 @@ export default defineComponent({
 
       this.emitFilterChange();
     },
+
     emitFilterChange(): void {
       const filters: Record<string, [Condition, Filter['value']]> = {};
+
+      // Build filters from newFilters (source of truth)
       for (const { condition, value, fieldname } of this.newFilters) {
         if (value === '' && condition) {
           continue;
         }
 
-        filters[fieldname] = [condition, value];
+        if (fieldname === 'numberSeries') {
+          filters['name'] = [condition, value];
+        } else {
+          filters[fieldname] = [condition, value];
+        }
       }
 
       this.$emit('change', filters);
+      this.filters = [...this.newFilters];
+
       if (this.newFilters.length) {
         this.filters = this.filters.filter(
           (filter) => filter.condition && filter.value && filter.fieldname
         );
-
         this.filters.push(this.newFilters[this.newFilters.length - 1]);
       }
 
+      // Deduplicate filters
       this.filters = Array.from(
         new Map(
           this.filters.map((filter) => [
