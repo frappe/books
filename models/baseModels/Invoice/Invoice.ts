0fc18bd934889dd2a5bb6524dd2c0ea2c1575e33
Gadha Venu <gadhavenu2311@gmail.com>
2025-05-07 12:04:53 +0530

    revert

--- models/baseModels/Invoice/Invoice.ts
+++ models/baseModels/Invoice/Invoice.ts
@@ -195,6 +195,18 @@ export abstract class Invoice extends Transactional {
     await super.afterSubmit();
 
     if (this.isReturn) {
+    
+      await this.fyo.db.update(this.schemaName,{
+        name:this.name as string,
+        outstandingAmount:0
+      })
+    
+      const party = (await this.fyo.doc.getDoc(
+        ModelNameEnum.Party,
+        this.party
+      )) as Party;
+  
+      await party.updateOutstandingAmount();
       await this._removeLoyaltyPointEntry();
       this.reduceUsedCountOfCoupons();
 
@@ -921,7 +933,7 @@ export abstract class Invoice extends Transactional {
       this.exchangeRate === 1 || this.baseGrandTotal!.isZero(),
     stockNotTransferred: () => !this.stockNotTransferred,
     outstandingAmount: () =>
-      !!this.outstandingAmount?.isZero() || !this.isSubmitted,
+      !this.isSubmitted,
     terms: () => !(this.terms || !(this.isSubmitted || this.isCancelled)),
     attachment: () =>
       !(this.attachment || !(this.isSubmitted || this.isCancelled)),
