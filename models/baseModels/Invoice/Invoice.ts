20cd3624885e194b7fa5bdb2939d1699dc851efb
Gadha Venu <gadhavenu2311@gmail.com>
2025-05-08 09:41:20 +0530

    fix:outstanding amount zero without payment

--- models/baseModels/Invoice/Invoice.ts
+++ models/baseModels/Invoice/Invoice.ts
@@ -195,18 +195,6 @@ export abstract class Invoice extends Transactional {
     await super.afterSubmit();
 
     if (this.isReturn) {
-    
-      await this.fyo.db.update(this.schemaName,{
-        name:this.name as string,
-        outstandingAmount:0
-      })
-    
-      const party = (await this.fyo.doc.getDoc(
-        ModelNameEnum.Party,
-        this.party
-      )) as Party;
-  
-      await party.updateOutstandingAmount();
       await this._removeLoyaltyPointEntry();
       this.reduceUsedCountOfCoupons();
 
@@ -824,6 +812,18 @@ export abstract class Invoice extends Transactional {
         if (this.submitted) {
           return;
         }
+        if (this.isReturn) {
+          const originalInvoice = await this.fyo.doc.getDoc(
+            this.schemaName, 
+            this.returnAgainst!
+          ) as Invoice;
+          if (originalInvoice.outstandingAmount?.isZero()) {
+            return this.grandTotal;
+          } else {
+            return this.fyo.pesa(0);
+          }
+        }
+
         if (this.redeemLoyaltyPoints) {
           return await this.getLPAddedBaseGrandTotal();
         }
