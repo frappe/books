15ec5d6c446b88456c7883484b7066951ed3422b
Tony <tony@tonydarcy.online>
2025-11-19 22:13:12 +1100

    Allow Editing of Submitted Journal Transaction If The Type Is Bank Entry
    Allow Editing of Submitted Journal Transaction If The Type Is Bank Entry.
    This allows transactions to be allocated after they have been imported instead of just sitting in the suspense account.

--- models/baseModels/JournalEntryAccount/JournalEntryAccount.ts
+++ models/baseModels/JournalEntryAccount/JournalEntryAccount.ts
@@ -1,5 +1,5 @@
 import { Doc } from 'fyo/model/doc';
-import { FiltersMap, FormulaMap } from 'fyo/model/types';
+import { FiltersMap, FormulaMap, ReadOnlyMap } from 'fyo/model/types';
 import { Money } from 'pesa';
 
 export class JournalEntryAccount extends Doc {
@@ -26,6 +26,28 @@ export class JournalEntryAccount extends Doc {
       return totalOtherType.sub(totalType);
     }
   }
+
+  /**
+   * Overridden to defer editability to the parent document.
+   * If the parent JournalEntry is editable (e.g., it's a Bank Entry),
+   * then this row should also be editable.
+   */
+  get canEdit() {
+    if (this.parentdoc) {
+      return this.parentdoc.canEdit;
+    }
+    return true;
+  }
+
+  /**
+   * Explicitly define fields as not read-only when the document is editable.
+   * This prevents the UI from locking the 'account' field on submitted docs.
+   */
+  readOnly: ReadOnlyMap = {
+    account: () => !this.canEdit,
+    debit: () => !this.canEdit,
+    credit: () => !this.canEdit,
+  };
 
   formulas: FormulaMap = {
     debit: {
