db738d26a7f74d021d5d1f78c6ad26f7f404fe45
Will Neill <willneill@gmail.com>
2026-02-16 22:15:00 +0100

    feat: add Australian GST tax templates
    Create regional setup for Australia that automatically configures
    GST-10 (10% standard rate) and GST-Free (0%) tax templates during
    company setup. Follows the established pattern used by India, Czech
    Republic, and Slovakia.
    
    Closes #1358

--- src/regional/index.ts
+++ src/regional/index.ts
@@ -1,7 +1,12 @@
 import { Fyo } from 'fyo';
+import { createAustralianRecords } from './au/au';
 import { createIndianRecords } from './in/in';
 
 export async function createRegionalRecords(country: string, fyo: Fyo) {
+  if (country === 'Australia') {
+    await createAustralianRecords(fyo);
+  }
+
   if (country === 'India') {
     await createIndianRecords(fyo);
   }
