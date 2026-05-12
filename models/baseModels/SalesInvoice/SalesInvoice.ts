7ed36d2b9d4d992c85c0f9cad37d54d353317e82
Charles Nkonoki <charlitokenn@gmail.com>
2026-05-12 23:15:04 +0300

    fix: change variable declaration from let to const for productListItems in SalesInvoice

--- models/baseModels/SalesInvoice/SalesInvoice.ts
+++ models/baseModels/SalesInvoice/SalesInvoice.ts
@@ -179,7 +179,7 @@ export class SalesInvoice extends Invoice {
         )
       );
 
-      let productListItems = [];
+      const productListItems = [];
       for (let index = 0; index < (this.items ?? []).length; index++) {
         const item = this.items![index];
         const itemName = item.item as string;
